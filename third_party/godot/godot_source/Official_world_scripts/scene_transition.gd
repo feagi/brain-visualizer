417afeb7e80a9fc13037a5e8aea17776dff2943c
kakcalu13 <kevin.a.araujo@gmail.com>
2023-06-26 11:44:47 -0400

    remove old menu completely

--- third_party/godot/godot_source/Official_world_scripts/scene_transition.gd
+++ third_party/godot/godot_source/Official_world_scripts/scene_transition.gd
@@ -1,9 +1,5 @@
 extends Node3D
 
-func ready():
-	$Button.position.y = $Menu/information_menu.position.y
-	
-
 func change_scene():
 	var BV = $Core/GlobalUISystem/Brain_Visualizer
 	var CB = $Core/GlobalUISystem/graphCore
@@ -11,12 +7,10 @@ func change_scene():
 	await $scene_transition/AnimationPlayer.animation_finished
 	if BV.visible:
 		BV.visible = false
-		$Menu.visible = false
 		CB.visible = true
 		$Button.text = "Brain Visualizer"
 	elif CB.visible:
 		BV.visible = true
-		$Menu.visible = true
 		CB.visible = false
 		$Button.text = "Circuit Builder"
 	$scene_transition/AnimationPlayer.play_backwards("dissolve", -1)
@@ -24,4 +18,3 @@ func change_scene():
 func _on_button_pressed():
 	change_scene()
 	$Button.release_focus()
-#	print("Here: ", $Control.TopBar.componentData)
