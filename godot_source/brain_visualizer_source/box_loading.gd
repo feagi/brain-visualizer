bc653912d43ea55ccc9ae74c0de35efff2a7d1ae
kakcalu13 <kevin.a.araujo@gmail.com>
2023-06-26 14:07:30 -0400

    surpress warning

--- godot_source/brain_visualizer_source/box_loading.gd
+++ godot_source/brain_visualizer_source/box_loading.gd
@@ -1,7 +1,7 @@
 extends ColorRect
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if timer_api.loading_box_timer:
 		visible = true
 	else:
