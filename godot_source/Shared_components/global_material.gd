f5b700a3758b6728d11891b8d2714b78443fc354
kakcalu13 <kevin.a.araujo@gmail.com>
2023-06-30 14:11:25 -0400

    add destination as global

--- godot_source/Shared_components/global_material.gd
+++ godot_source/Shared_components/global_material.gd
@@ -5,6 +5,7 @@ extends Node
 @onready var deselected = preload("res://brain_visualizer_source/cortical_area.material")
 @onready var selected = preload("res://brain_visualizer_source/selected.material")
 @onready var glow = preload("res://brain_visualizer_source/glow.material")
+@onready var destination = preload("res://brain_visualizer_source/destination.material")
 
 func ready():
 	print("material created and ready now.")
