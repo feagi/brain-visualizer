fe7fd1c2042b59da692f463af45e6e8294af3614
kakcalu13 <kevin.a.araujo@gmail.com>
2023-06-30 12:52:33 -0400

    added glow in global

--- godot_source/Shared_components/global_material.gd
+++ godot_source/Shared_components/global_material.gd
@@ -4,6 +4,7 @@ extends Node
 @onready var white = preload("res://brain_visualizer_source/white.material")
 @onready var deselected = preload("res://brain_visualizer_source/cortical_area.material")
 @onready var selected = preload("res://brain_visualizer_source/selected.material")
+@onready var glow = preload("res://brain_visualizer_source/glow.material")
 
 func ready():
 	print("material created and ready now.")
