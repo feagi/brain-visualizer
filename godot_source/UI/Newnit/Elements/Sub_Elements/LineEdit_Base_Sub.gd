bbbf3b6dfa4b7a4ca5456c2ae2133a7fd78a6ab6
Amir-Rasteg <127231771+Amir-Rasteg@users.noreply.github.com>
2023-07-03 13:16:33 -0400

    Expanded Base to have required signals

--- godot_source/UI/Newnit/Elements/Sub_Elements/LineEdit_Base_Sub.gd
+++ godot_source/UI/Newnit/Elements/Sub_Elements/LineEdit_Base_Sub.gd
@@ -1,9 +1,10 @@
 extends LineEdit
-class_name  LineEdit_Base
+class_name  LineEdit_Base_Sub
 # Base script for all LineEdit based subcomponents, do not create directly
 
 const MAX_CHARACTERS_SHOWN = 8
 const LIMIT_MAX_CHARACTERS_LENGTH = false
+const HORIZONTAL_ALIGNMENT = 1
 
 signal value_edited(newString: String)
 
@@ -13,12 +14,18 @@ var minWidth: float:
 var prefix: String = ""
 var suffix: String = ""
 var rootText: String = ""
+var _cachedRootText: String = ""
 
 # Sets the text property of the line edit after proper filters
 func SetText(input: String, backup: String) -> void:
 	rootText = _FilterText(input, backup)
+	_emitValChangedIfChanged(rootText)
 	text = prefix + rootText + suffix
 
+func _emitValChangedIfChanged(newRootText: String) -> void:
+	if _cachedRootText != newRootText:
+		_cachedRootText = newRootText
+		value_edited.emit(newRootText)
 
 # Function to filter inplausible text, override in child classes
 func _FilterText(input: String, _replacementIncorrect: String) -> String:
@@ -26,6 +33,7 @@ func _FilterText(input: String, _replacementIncorrect: String) -> String:
 
 # TODO this camera focusing system is flawed, and should be replaced
 func _ready():
+	alignment = HORIZONTAL_ALIGNMENT
 	mouse_entered.connect(_toggleCamUsageOn)
 	mouse_exited.connect(_toggleCamUsageOff)
 
