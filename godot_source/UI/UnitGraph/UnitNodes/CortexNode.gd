f1573010715a3396ec1d2e8568a61735a913c706
Amir-Rasteg <127231771+Amir-Rasteg@users.noreply.github.com>
2023-07-06 13:30:09 -0400

    Brought Cortex Node up to date with current standards

--- godot_source/UI/UnitGraph/UnitNodes/CortexNode.gd
+++ godot_source/UI/UnitGraph/UnitNodes/CortexNode.gd
@@ -7,15 +7,29 @@ var friendlyName: String:
 	set(v): _Label.text = v
 	get: return _Label.text
 
-var _Label
+var corticalID: String
+var corticalType: REF.CORTICALTYPE
+var usingCustomLocation: bool = false
 
-# Signals
+var _Label: Label_Sub
 
 
-func _ready():
-	_Label = $Label
+func _init(cortexID: String, cortexOverview: Dictionary):
+	super()
+	_Label = Label_Sub.new()
+	add_child(_Label)
+	
+	corticalID = cortexID; name = cortexID
+	friendlyName = cortexOverview["friendlyName"]
+	if "position" in cortexOverview.keys():
+		position_offset = cortexOverview["position"]
+		usingCustomLocation = true
+	
 	set_slot_type_left(0, int(CONNECTIONTYPES.Default))
 	set_slot_type_right(0, int(CONNECTIONTYPES.Default))
+
+
+
 
 
 
