ed1ec2d768acc035c61a8e6b72bdd94f8fb53c8f
Amir Rasteg <amir.rasteg@protonmail.com>
2023-07-20 17:44:41 -0400

    Start Window Manager

--- godot_source/UI/Windows/WindowManager.gd
+++ godot_source/UI/Windows/WindowManager.gd
@@ -1,1 +1,16 @@
 extends Object
+class_name WindowManager
+
+var UIMan: UI_Manager
+var MappingDefinition: Window_MappingDefinition
+var currentLanguageISO: StringName
+
+
+func _init(UIManager: UI_Manager, languageCode: StringName):
+	
+	UIMan = UIManager
+	currentLanguageISO = languageCode
+	# Prep Mapping Definition
+	
+	MappingDefinition = Window_MappingDefinition.new("Newnit_Popup", HelperFuncs.GenerateDefinedUnitDict("MAPPING_DEFINITION", currentLanguageISO), UIMan)
+
