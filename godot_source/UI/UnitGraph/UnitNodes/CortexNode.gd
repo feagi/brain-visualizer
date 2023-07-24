770dec5706b21227f03480ecb08c9f0308c9bf63
Amir Rasteg <amir.rasteg@protonmail.com>
2023-07-24 13:36:05 -0400

    Update CortexNode to not use Dictioanries for Init

--- godot_source/UI/UnitGraph/UnitNodes/CortexNode.gd
+++ godot_source/UI/UnitGraph/UnitNodes/CortexNode.gd
@@ -7,37 +7,41 @@ var friendlyName: String:
 	set(v): _Label.text = v
 	get: return _Label.text
 
-var corticalID: String:
+var corticalID: CortexID:
 	get: return _corticalID
 	set(v):
 		_corticalID = v
-		title = v
-		name = v
-		_Label.name = v
+		title = v.str
+		name = v.str
+		_Label.name = v.str
 		
 var corticalType: REF.CORTICALTYPE
 var usingCustomLocation: bool = false
 
-var _corticalID: String
+var _corticalID: CortexID
 var _Label: Label_Sub
 
-
-func _init(cortexID: String, cortexOverview: Dictionary):
+# We have to use a positionArr because gdscript doesn't allow null statics
+func _init(cortexID: CortexID, niceName: String, positionArr: PackedInt32Array = []):
 	super()
 	_Label = Label_Sub.new()
 	add_child(_Label)
 	
 	corticalID = cortexID
-	friendlyName = cortexOverview["friendlyName"]
-	if "position" in cortexOverview.keys():
-		position_offset = cortexOverview["position"]
-		usingCustomLocation = true
+	friendlyName = niceName
 	
 	set_slot_enabled_left(0, true)
 	set_slot_enabled_right(0, true)
 	
 	set_slot_type_left(0, int(CONNECTIONTYPES.Default))
 	set_slot_type_right(0, int(CONNECTIONTYPES.Default))
+	
+	if positionArr.size() == 0: return
+		
+	position_offset = HelperFuncs.Array2Vector2i(positionArr)
+	usingCustomLocation = true
+	
+
 
 
 
