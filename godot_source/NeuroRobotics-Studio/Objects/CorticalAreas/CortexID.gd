5605d54239963bcac727f411a21530be7b118c4c
Amir Rasteg <amir.rasteg@protonmail.com>
2023-07-25 16:26:53 -0400

    Optimized CortexID and added Future note

--- godot_source/NeuroRobotics-Studio/Objects/CorticalAreas/CortexID.gd
+++ godot_source/NeuroRobotics-Studio/Objects/CorticalAreas/CortexID.gd
@@ -4,13 +4,14 @@ class_name CortexID
 # This script is just to segregate CortexIDs from String
 # Every time you open this file, a functional programmer is born
 
-var ID: String:
-	get: return _ID
-	
-var str: String:
+var ID: StringName:
 	get: return _ID
 
-var _ID: String
+#TODO we probably shouldn't be calling this str
+var str: StringName:
+	get: return _ID
 
-func _init(id: String):
+var _ID: StringName
+
+func _init(id: StringName):
 	_ID = id
