a364527066e23a5c8914f0435f7fdd85dd320a88
Amir Rasteg <amir.rasteg@protonmail.com>
2023-07-25 16:22:45 -0400

    Fully moved Graph Core to new System. For real this time!

--- godot_source/UI/UnitGraph/GraphCore.gd
+++ godot_source/UI/UnitGraph/GraphCore.gd
@@ -1,80 +1,85 @@
 extends GraphEdit
 class_name GraphCore
 
-signal DataUp(data: Dictionary)
+signal CortexSelected(cortex: CortexID)
+signal ConnectionSelected(sourceNode: CortexID, destinationNode: CortexID)
+signal ConnectionRequest(sourceNode: CortexID, destinationNode: CortexID)
+signal DisconnectionRequest(sourceNode: CortexID, destinationNode: CortexID)
 
-var isActivated := false
 
-const DEFAULT_SPAWN_WIDTH = 150.0
-const DEFAULT_HEIGHT_GAP = 10.0
-
+const DEFAULT_SPAWN_WIDTH: int = 150
+const DEFAULT_HEIGHT_GAP: int = 10
 
 var cortexNodes: Dictionary # CortexIDStr -> CortexNode
 var connectionLabels: Dictionary # srcCortexIDStr -> dstCortexIDStr -> ConnectionLabel
 # NOTE, THe above data structure is not efficient, REPLACE
 var _UIManRef: UI_Manager
 
-
 func _ready():
-	FeagiVarUpdates.Internal_corticalMapSummary.connect(_SpawnNodesFromFullCorticalData) # TODO this may not want to be here
-	arrange_nodes_button_hidden = true
+	FeagiVarUpdates.Internal_corticalMapSummary.connect(_SpawnNodesFromFullCorticalData)
+
 	connection_request.connect(_ConnectingNodesTogether)
-	self.node_selected.connect(_NodeSelected)
-	#right_disconnects = true # TODO NEXT
+	disconnection_request.connect(_DisconnectingNodesFromEachOther)
+	node_selected.connect(_NodeSelected)
+
+	arrange_nodes_button_hidden = true
+	right_disconnects = true
 	_UIManRef = get_parent()
 
 
 
-# Handles Recieving data from UI Manager, and distributing it to the correct element
-# DEPRECATED: TODO REMOVE
-func RelayDownwards(callType, data: Dictionary = {}):
-	match(callType):
-		REF.FROM.godot_fullCorticalData:
-			_SpawnNodesFromFullCorticalData(data) # TODO - add startup check
-			pass
+####################################
+############ User Input ############
+####################################
+
+# Proxy for Node Selection Event
+func _NodeSelected(nodeReference: CortexNode) -> void:
+	CortexSelected.emit(nodeReference.corticalID)
+
+func _ConnectionLabelPressed(label: Connection_Label) -> void:
+	ConnectionSelected.emit(label.sourceNode.corticalID, label.destinationNode.corticalID)
+
+# Proxy for signal for user connecting nodes
+func _ConnectingNodesTogether(sourceNodeID: String, _fromPort: int, destNodeID: String, _toPort: int) -> void:
+	ConnectionRequest.emit(CortexID.new(sourceNodeID), CortexID.new(destNodeID))
+
+# Proxy for signal for user disconnecting nodes
+func _DisconnectingNodesFromEachOther(sourceNodeID: String, _fromPort: int, destNodeID: String, _toPort: int) -> void:
+	DisconnectionRequest.emit(CortexID.new(sourceNodeID), CortexID.new(destNodeID))
+
 
 ####################################
 ###### Single Entity Handling ######
 ####################################
 
 # Spawns a individual node with its required settings (not connections)
-func SpawnCorticalNode(ID: CortexID, friendlyName: String, positionArr: PackedInt32Array = []) -> CortexNode:
-	var newNode: CortexNode = CortexNode.new(ID, friendlyName, positionArr)
-	add_child(newNode)
+func SpawnCorticalNode(ID: CortexID, friendlyName: String, positionFromFeagi: Vector2i, isPositionDefinedFromFeagi: bool) -> CortexNode:
+	var newNode: CortexNode = CortexNode.new(ID, friendlyName, isPositionDefinedFromFeagi, self, positionFromFeagi)
 	cortexNodes[ID.str] = newNode
 	return newNode
 
-func CreateVisibleConnection(srcNode: CortexNode, dstNode: CortexNode, numConnections: int) -> void:
-	_VisuallyConnectNodes(srcNode.corticalID, dstNode.corticalID)
+# Spawns a Label (which also automatically negotiates a connection line) connecting 2 nodes together
+func CreateVisibleConnection(srcNode: CortexNode, dstNode: CortexNode, numConnections: int) -> Connection_Label:
 	var newLabel: Connection_Label = Connection_Label.new(srcNode, dstNode, numConnections, self)
-	
+	newLabel.ButtonPressed.connect(_ConnectionLabelPressed)
+
 	if srcNode.corticalID.str not in connectionLabels.keys():
 		connectionLabels[srcNode.corticalID.str] = {dstNode.corticalID.str: newLabel}
 	else:
 		connectionLabels[srcNode.corticalID.str][dstNode.corticalID.str] = newLabel
+	return newLabel
 	
 
-# Creates the visible line connection. Called from "connection_request" signal
-func _VisuallyConnectNodes(fromNode: CortexID, toNode: CortexID, fromPort: int = 0, toPort: int = 0) -> void:
-	connect_node(fromNode.str, fromPort, toNode.str, toPort)
+func FullyRemoveConnection(srcNode: CortexNode, dstNode: CortexNode) -> void:
+	var label: Connection_Label = connectionLabels[srcNode.corticalID.str][dstNode.corticalID.str]
+	label.DestroyConnection()
+	connectionLabels[srcNode.corticalID.str].erase(dstNode.corticalID.str)
+	if connectionLabels[srcNode.corticalID.str] == {}:
+		connectionLabels.erase(srcNode.corticalID.str)
 
-# Proxy for signal for user connecting nodes
-func _ConnectingNodesTogether(sourceNodeID: String, _fromPort: int, destNodeID: String, _toPort: int):
-	var sourceNode: CortexNode = cortexNodes[sourceNodeID]
-	var destNode: CortexNode = cortexNodes[destNodeID]
-	var data := {
-	"event": "NodesLinked",
-	"source": sourceNode.friendlyName,
-	"destination": destNode.friendlyName
-	}
-	DataUp.emit(data)
 
-func _ProcessConnectionButtonPress(data: Dictionary):
-	DataUp.emit(data)
 
-# Handles Node Selection Event
-func _NodeSelected(nodeReference):
-	DataUp.emit({"CortexSelected": nodeReference.name})
+
 
 
 ####################################
@@ -83,37 +88,46 @@ func _NodeSelected(nodeReference):
 
 # Assuming a blank grid, spawn nodes with connections as per most recently cached FEAGI state
 func _SpawnNodesFromFullCorticalData(fullCorticalData: Dictionary) -> void:
+	
+	# looped vars up here to reduce GC
+	var curCortexID: CortexID
 	var cortexContext: Dictionary
+	var spawnedNode: CortexNode
+	var cortexType: StringName
+	var cortexTypeIndex: int
+
+	
+
+	
 	
 	var numColumns = REF.CORTICALTYPE.size()
-	var widths: PackedFloat32Array; widths.resize(numColumns)
-	var heights: PackedFloat32Array; heights.resize(numColumns)
-	heights.fill(0.0)
+	var widths: PackedInt32Array = []
+	widths.resize(numColumns)
+	var heights: PackedInt32Array = []
+	heights.resize(numColumns)
+	heights.fill(0)
 	for i in range(numColumns):
 		widths[i] = ((DEFAULT_SPAWN_WIDTH / -2) * numColumns) + (i * DEFAULT_SPAWN_WIDTH)
 	
-	
 	for cortexIDStr in fullCorticalData.keys():
-		var curCortexID: CortexID = CortexID.new(cortexIDStr)
+		curCortexID = CortexID.new(cortexIDStr)
 		cortexContext = fullCorticalData[curCortexID.str]
-		var spawnedNode = SpawnCorticalNode(curCortexID, cortexContext["friendlyName"])
-		var type: String = cortexContext.type.to_upper()
-		var nodeCategoryIndex: int = REF.CORTICALTYPE[type]
-		if "position" not in cortexContext.keys():
-			spawnedNode.position_offset = Vector2(widths[nodeCategoryIndex], heights[nodeCategoryIndex])
-			heights[nodeCategoryIndex] = heights[nodeCategoryIndex] + spawnedNode.size.y + DEFAULT_HEIGHT_GAP
-		else:
-			spawnedNode.position_offset = cortexContext["position"]
+		cortexType = cortexContext.type.to_upper()
+		cortexTypeIndex = REF.CORTICALTYPE[cortexType]
 
+		if "position" not in cortexContext.keys():
+			spawnedNode = SpawnCorticalNode(curCortexID, cortexContext["friendlyName"], Vector2i(widths[cortexTypeIndex], heights[cortexTypeIndex]), false)
+			heights[cortexTypeIndex] = heights[cortexTypeIndex] + int(spawnedNode.size.y) + DEFAULT_HEIGHT_GAP
+		else:
+			spawnedNode = SpawnCorticalNode(curCortexID, cortexContext["friendlyName"], cortexContext["position"], true)
 
 	var connectionCount: int  # init here to reduce GC
 	var _srcCortex: CortexNode # init here to reduce GC
 	var _dstCortex: CortexNode # init here to reduce GC
 	# This loop runs under the assumption that the connectome mapping only shows in -> out
 	# Yes we need a seperate for loop for this. Too Bad!
-	for cortexIDStr in fullCorticalData.keys():
-		var curCortexID: CortexID = CortexID.new(cortexIDStr)
-		cortexContext = fullCorticalData[curCortexID.str]
+	for srcCortexIDStr in fullCorticalData.keys():
+		cortexContext = fullCorticalData[srcCortexIDStr]
 		if cortexContext["connectedTo"] != {}:
 			# we have connections to map
 			for connectionName in cortexContext["connectedTo"].keys():
