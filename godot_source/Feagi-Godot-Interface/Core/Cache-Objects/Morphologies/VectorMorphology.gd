b2443ebc6ad940b69de7a8a43eac630c5502ca0d
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-07 16:45:16 -0400

    Add Vector Morphology

---
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Morphologies/VectorMorphology.gd
@@ -0,0 +1,11 @@
+extends Morphology
+class_name VectorMorphology
+## Morphology of type Vector
+
+var vectors: Array[Vector3i]
+# PackedVector3iArrays do not exist as per https://github.com/godotengine/godot/pull/66616
+# While a similar effect can be emulated using a PackedInt32 array and a Stride of 3, these arrays are so small it likely isn't worth the effort for such minimal memory savings
+
+func _init(morphology_name: StringName, morphology_type: TYPE, morphology_vectors: Array[Vector3i]):
+    super(morphology_name, morphology_type)
+    vectors = morphology_vectors
