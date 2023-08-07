7cf782b7739ee195de8885610040ef38f026e85c
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-07 18:00:17 -0400

    Create PatternVector3Pairs for Pattern Morphology

---
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Morphologies/Pattern-Variables/PatternVector3Pairs.gd
@@ -0,0 +1,10 @@
+extends Object
+class_name PatternVector3Pairs
+## Organizes PatternVector3s into in/out pairs, which is used in [PatternMorphology]
+
+var incoming: PatternVector3
+var outgoing: PatternVector3
+
+func _init(going_in: PatternVector3, going_out: PatternVector3):
+    incoming = going_in
+    outgoing = going_out
