1e1886da35dad96c00f92425fbab0c798016b35e
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-08 14:51:04 -0400

    Update GEAGIInterface to use seperate network start function

--- godot_source/Feagi-Godot-Interface/Core/FEAGIInterface.gd
+++ godot_source/Feagi-Godot-Interface/Core/FEAGIInterface.gd
@@ -9,11 +9,19 @@ var _response_calls: ResponseProxyFunctions
 
 
 func _init():
+	# Init Network Objects
 	net = NetworkInterface.new()
 	net.init_network(self) # Grabs addresses and initializes connection details
 	_response_calls = ResponseProxyFunctions.new()
 	calls = CallList.new(net, _response_calls)
-
-func _ready():
 	FeagiRequests._feagi_interface = self
+
+## Runs after _init
+func _ready():
+	initial_FEAGI_calls()
+
+
+## Calls to be made to FEAGI following initialization
+## Put any calls here that need to be made for initial summary data to spawn initial UI elements
+func initial_FEAGI_calls() -> void:
 	FeagiRequests.refresh_morphology_list()
