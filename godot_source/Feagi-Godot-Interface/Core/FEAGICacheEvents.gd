ad8b0cba71a2a1c58f7e7f9f711ae5507d557e4a
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-10 14:11:27 -0400

    Add morphology event signals

--- godot_source/Feagi-Godot-Interface/Core/FEAGICacheEvents.gd
+++ godot_source/Feagi-Godot-Interface/Core/FEAGICacheEvents.gd
@@ -1,3 +1,11 @@
 extends Node
 ## AUTOLOADED
-## Essentially a collection of signals when a change in FEAGI state (or dependent internal variables) change
+## Essentially a collection of signals when a change in cached FEAGI state (or dependent internal variables) change
+
+
+################################# Morphologies ##################################
+signal morphology_added(new_morphology: Morphology) # For when singular morphology is added
+signal morphology_removed(removed_morphology: Morphology)  # For when singular morphology is added
+signal morphology_updated(updated_morphology: Morphology) # For when a morphology is updated in cache (COMMON USE: morphology objects are init'd with placeholder values until FEAGI responds with the correct one)
+
+
