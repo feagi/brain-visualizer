fb8d72aabcd1b86781133c95a4e7af3f698948ca
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-14 17:49:41 -0400

    commit in case next change attempt needs backtracking

--- godot_source/Feagi-Godot-Interface/UI/InputInterpreter.gd
+++ godot_source/Feagi-Godot-Interface/UI/InputInterpreter.gd
@@ -2,12 +2,19 @@ extends Node
 class_name InputInterpreter
 ## Translates input events from mouse / touch into easier use signal outputs
 
+
 signal zoom_changed(new_zoom: float)
 signal pan_changed(new_pan: Vector2)  # Random Crits not included
 
-var zoom_limit_upper: float = 10
-var zoom_limit_lower: float = 0.1
-var pan_speed: float = 1
+@export var zoom_limit_upper: float = 10
+@export var zoom_limit_lower: float = 0.1
+@export var pan_speed: float = 1
+
+@export var mouse_normal_click_button: MouseButton = MOUSE_BUTTON_LEFT
+@export var mouse_alt_click_button: MouseButton = MOUSE_BUTTON_RIGHT
+@export var mouse_pan_button: MouseButton = MOUSE_BUTTON_MIDDLE
+
+
 
 
 var touching_points: Dictionary = {} # what fingers are touching (in case of mouse, theres only 1)
@@ -32,18 +39,20 @@ var _pan_current: Vector2 = Vector2(0.0,0.0)
 
 func _input(event):
 
-	match event:
-		# Query if its a touch event first
-		InputEventScreenTouch:
-			_handle_press_touch(event)
-		InputEventScreenDrag:
-			pass
-		
-		# Query if its a mouse event
-		InputEventMouseButton:
-			pass
-		InputEventMouseMotion:
-			pass
+	# Query if its a touch event first
+	if event is InputEventScreenTouch:
+		_handle_press_touch(event)
+
+	# Query if its a mouse event
+	if event is InputEventMouseButton:
+		_handle_click(event)
+
+	# Screen Dragging
+	if event is InputEventScreenDrag:
+		pass
+
+	if event is InputEventMouseMotion:
+		pass
 
 ## for responding to touch events (tap and double tap)
 func _handle_press_touch(event: InputEventScreenTouch) -> void:
@@ -69,8 +78,32 @@ func _handle_press_touch(event: InputEventScreenTouch) -> void:
 	if touching_points.size() < 2:
 		start_distance = 0.0
 
-## for responding to touch drag events
-func _handle_drag_touch(event: InputEventScreenDrag) -> void:
+## Responding to MouseClicks
+func _handle_click(event: InputEventMouseButton) -> void:
+
+	if event.double_click:
+		pass
+	
+	match event.button_index:
+		mouse_normal_click_button:
+			# clicking
+			pass
+
+		mouse_alt_click_button:
+			# alt (usually right) clicking
+			pass
+
+		mouse_pan_button:
+			# if pan button is selected
+			pass
+
+		MOUSE_BUTTON_NONE:
+			# let go
+			pass
+
+
+## for responding to  drag events
+func _handle_drag(event: InputEventScreenDrag) -> void:
 	_touching_points[event.index] = event.position
 
 	match _touching_points.size():
@@ -84,5 +117,7 @@ func _handle_drag_touch(event: InputEventScreenDrag) -> void:
 			var zoom_factor = start_distance / current_distance
 			zoom_current = FEAGIUtils.bounds(zoom_factor, zoom_limit_lower, zoom_limit_upper)
 
+
+	
 
 
