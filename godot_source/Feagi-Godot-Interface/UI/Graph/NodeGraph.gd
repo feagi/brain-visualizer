100b2375b97bf2b034aab56c50f2d219f2a89511
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-15 15:26:31 -0400

    Added Logo, disable zoom for now

--- godot_source/Feagi-Godot-Interface/UI/Graph/NodeGraph.gd
+++ godot_source/Feagi-Godot-Interface/UI/Graph/NodeGraph.gd
@@ -7,6 +7,7 @@ var panning: Vector2:
 		_panning = v
 		_apply_pan(v)
 
+
 var zoom: float:
 	get: return _zoom
 	set(v):
@@ -16,6 +17,7 @@ var zoom: float:
 var _input: InputInterpreter
 var _background: Control
 var _background_shader: Material
+var _logo: CBLogo
 
 var _panning: Vector2 = Vector2(0.0, 0.0)
 var _zoom: float = 1.0
@@ -24,6 +26,7 @@ func _ready():
 	_background = $Background
 	_background_shader = _background.material
 	_input = $InputInterpreter
+	_logo = $Logo
 	_input.pan_changed.connect(_apply_pan)
 	_input.zoom_changed.connect(_apply_zoom)
 	VisConfig.screen_size_changed.connect(_apply_resize)
@@ -32,11 +35,13 @@ func _ready():
 func _apply_pan(new_pan: Vector2) -> void:
 	_panning = new_pan
 	_background_shader.set_shader_parameter("offset", new_pan)
+	_logo.set_background_position(new_pan)
+	print(new_pan)
 
 func _apply_zoom(new_zoom: float) -> void:
-	_zoom = new_zoom
-	_background_shader.set_shader_parameter("zoom", new_zoom)
+	#_zoom = new_zoom
+	#_background_shader.set_shader_parameter("zoom", new_zoom)
+	pass
 
 func _apply_resize(new_size: Vector2) -> void:
 	_background.size = new_size
-	print(new_size)
