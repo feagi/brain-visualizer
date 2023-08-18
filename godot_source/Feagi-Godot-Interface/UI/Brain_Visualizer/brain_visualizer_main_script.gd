4841cc04a0a955e8d7e8842911a291b3d52d58f0
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-18 14:05:23 -0400

    expermenting section

--- godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/brain_visualizer_main_script.gd
+++ godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/brain_visualizer_main_script.gd
@@ -1,10 +1,9 @@
 extends Node3D
-
-
+var _shaderMat # EXPERIMENT
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	FeagiCacheEvents.cortical_area_added.connect(on_cortical_area_added)
-	print("done")
+	_shaderMat = $cortical_area_box.mesh.material # EXPERIMENT
 
 func on_cortical_area_added(cortical_area: CorticalArea) -> void:
 	generate_cortical_area(cortical_area)
@@ -14,5 +13,16 @@ func generate_cortical_area(cortical_area_data : CorticalArea):
 	new_node.visible = true
 	new_node.set_name(cortical_area_data.name)
 	new_node.scale = cortical_area_data.dimensions
-	new_node.transform.origin = Vector3(cortical_area_data.coordinates_3D) * Vector3	(1,1,-1)
+	new_node.transform.origin = Vector3(cortical_area_data.coordinates_3D) * Vector3(1,1,-1)
 	add_child(new_node)
+
+## EXPERMENTING SHADER ONLY. NOT GOING TO BE OFFICIAL CODE
+func _process(delta):
+	UpdateRenderShader()
+
+func UpdateRenderShader():
+	_shaderMat.set_shader_parameter("ballPos", 0.5)
+	_shaderMat.set_shader_parameter("paddlePosX", 0.0)
+	_shaderMat.set_shader_parameter("isCurrentlyShocking", false)
+
+## EXPERIMENTING ENDS
