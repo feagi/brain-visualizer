3d27dfe6a11c6d8892f4b34e4fe8ae9ac39b2294
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-18 14:08:23 -0400

    Halfway through adding websockets

--- godot_source/Feagi-Godot-Interface/Core/FEAGIEvents.gd
+++ godot_source/Feagi-Godot-Interface/Core/FEAGIEvents.gd
@@ -1,4 +1,7 @@
 extends Node
 ## AUTOLOADED
-## Essentially a collection of signals when a change FEAGI state (that is NOT cached) is detected._add_constant_central_force
+## Essentially a collection of signals when a change FEAGI state (that is NOT cached) is detected
 ## Primarily used in cases of getting most recent info from FEAGI
+
+signal genome_was_reset()
+signal retrieved_visualization_data(data: Array)
--- godot_source/Feagi-Godot-Interface/Core/Networking/NetworkInterface.gd
+++ godot_source/Feagi-Godot-Interface/Core/Networking/NetworkInterface.gd
@@ -5,13 +5,18 @@ extends Object
 ##
 class_name NetworkInterface
 
-
 # Static Network Configuration
 const DEF_MINWORKERSAVAILABLE: int = 5
 const DEF_HEADERSTOUSE: PackedStringArray = ["Content-Type: application/json"]
 const DEF_FEAGI_TLD: StringName = "127.0.0.1"
 const DEF_FEAGI_SSL: StringName = "http://"
+const DEF_SOCKET_SSL: StringName = "ws://"
 const DEF_WEB_PORT: int = 8000
+const DEF_SOCKET_PORT: int = 9050
+const DEF_SOCKET_MAX_QUEUED_PACKETS: int = 10000000
+const DEF_SOCKET_INBOUND_BUFFER_SIZE: int = 10000000
+
+signal socket_state_changed(state: WebSocketPeer.State)
 
 var num_workers_available: int:
 	get: return workers_available.size()
@@ -22,13 +27,17 @@ var feagi_web_port: int
 var feagi_socket_port: int
 var feagi_root_web_address: StringName
 var feagi_root_websocket_address: StringName
+var feagi_socket_SSL: StringName
+var feagi_socket_address: StringName
 var feagi_outgoing_headers: PackedStringArray
 var endpoints: AddressList
 var num_workers_to_keep_available: int
 var workers_available: Array[RequestWorker]
+var current_websocket_state: WebSocketPeer.State
 
 var _request_worker_parent: Node
 var _multithreading_enabled: bool # cannot be changed after init
+var _socket: WebSocketPeer
 
 ## Used to init the network interface
 ## Required before usage
@@ -50,46 +59,54 @@ func init_network(worker_parent_root: Node) -> void:
 	feagi_SSL = DEF_FEAGI_SSL
 	feagi_TLD = DEF_FEAGI_TLD
 	feagi_web_port = DEF_WEB_PORT
+	feagi_socket_port = DEF_SOCKET_PORT
+	feagi_socket_SSL = DEF_SOCKET_SSL
 	feagi_outgoing_headers = DEF_HEADERSTOUSE
 	num_workers_to_keep_available = DEF_MINWORKERSAVAILABLE
 
 
-	# With collected data, init what we can
+	# With collected data, init API
 	_request_worker_parent = worker_parent_root
 	feagi_root_web_address = feagi_SSL + feagi_TLD + ":" + str(feagi_web_port)
+
 	endpoints = AddressList.new(feagi_root_web_address)
 	_spawn_initial_workers()
 	_log_connection_address()
 
+	# init WebSocket
+	feagi_socket_address = feagi_socket_SSL + feagi_TLD + ":" + str(feagi_socket_port)
+	_socket = WebSocketPeer.new()
+	_socket.connect_to_url(feagi_socket_address)
+	
+
+
+
 
 ## Makes a GET API call to FEAGI which then runs the given function call (with optional pass through data)
 func FEAGI_GET(full_request_address: StringName, function_to_respond_to_FEAGI: Callable, data_to_pass_through: Variant = null) -> void:
-	_call_FEAGI(full_request_address, HTTPClient.METHOD_GET , function_to_respond_to_FEAGI, null, data_to_pass_through)
+	_API_FEAGI(full_request_address, HTTPClient.METHOD_GET , function_to_respond_to_FEAGI, null, data_to_pass_through)
 
 
 ## Makes a POST API call to FEAGI with given data which then runs the given function call (with optional pass through data)
 func FEAGI_POST(full_request_address: StringName, function_to_respond_to_FEAGI: Callable, additional_data: Variant, data_to_pass_through: Variant = null) -> void:
-	_call_FEAGI(full_request_address, HTTPClient.METHOD_POST , function_to_respond_to_FEAGI, additional_data, data_to_pass_through)
+	_API_FEAGI(full_request_address, HTTPClient.METHOD_POST , function_to_respond_to_FEAGI, additional_data, data_to_pass_through)
 
 
 ## Makes a PUT API call to FEAGI with given data which then runs the given function call (with optional pass through data)
 func FEAGI_PUT(full_request_address: StringName, function_to_respond_to_FEAGI: Callable, additional_data: Variant, data_to_pass_through: Variant = null) -> void:
-	_call_FEAGI(full_request_address, HTTPClient.METHOD_PUT , function_to_respond_to_FEAGI, additional_data, data_to_pass_through)
+	_API_FEAGI(full_request_address, HTTPClient.METHOD_PUT , function_to_respond_to_FEAGI, additional_data, data_to_pass_through)
 
 
 ## Makes a DELETE API call to FEAGI with given data which then runs the given function call (with optional pass through data)
 func FEAGI_DELETE(full_request_address: StringName, function_to_respond_to_FEAGI: Callable, data_to_pass_through: Variant = null) -> void:
-	_call_FEAGI(full_request_address, HTTPClient.METHOD_DELETE , function_to_respond_to_FEAGI, null, data_to_pass_through)
-
-
+	_API_FEAGI(full_request_address, HTTPClient.METHOD_DELETE , function_to_respond_to_FEAGI, null, data_to_pass_through)
 
 
 ## Makes a API call using an available [RequestWorker] (or if none are available, spawns one first)
-func _call_FEAGI(full_request_address: StringName, method: HTTPClient.Method, 
+func _API_FEAGI(full_request_address: StringName, method: HTTPClient.Method, 
 	function_to_respond_to_FEAGI: Callable, additional_data: Variant = null, data_to_pass_through: Variant = null) -> void:
 
 	var worker: RequestWorker
-	var a = workers_available
 	if num_workers_available > 0:
 		worker = workers_available.pop_back()
 	else:
@@ -112,4 +129,27 @@ func _spawn_initial_workers() -> void:
 func _log_connection_address() -> void:
 	print("Using FEAGI address " + feagi_root_web_address)
 
+## Prints Websocket connection information to log
+func _log_socket_address() -> void:
+	print("Using FEAGI websocket address " + feagi_socket_address)
 
+
+## responsible for polling state of websocket, since its not event driven
+func _socket_status_poll() -> void:
+	_socket.poll()
+	match _get_socket_state():
+		WebSocketPeer.STATE_OPEN:
+			pass
+		
+
+
+
+## Queries and returns the current socket state. If its changed, updates the cached state and emits a signal of so
+func _get_socket_state() -> WebSocketPeer.State:
+	var new_state: WebSocketPeer.State = _socket.get_ready_state()
+	if new_state == current_websocket_state:
+		## no change, doesn't matter which we return
+		return new_state
+	socket_state_changed.emit(new_state)
+	current_websocket_state = new_state
+	return new_state
