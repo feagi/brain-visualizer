e10e44465abac0ba5d18ce31a4aafa1b95ffac5a
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-21 16:58:48 -0400

    updated path

--- godot_source/Feagi-Godot-Interface/UI/Resources/Shared_components/godot_material.gd
+++ godot_source/Feagi-Godot-Interface/UI/Resources/Shared_components/godot_material.gd
@@ -4,6 +4,6 @@ extends Node
 @onready var white = preload("res://Feagi-Godot-Interface/UI/Brain_Visualizer/assets/white.material")
 @onready var deselected = preload("res://Feagi-Godot-Interface/UI/Brain_Visualizer/assets/cortical_area.material")
 @onready var selected = preload("res://Feagi-Godot-Interface/UI/Brain_Visualizer/assets/selected.material")
-@onready var glow = preload("res://brain_visualizer_source/glow.material")
-@onready var destination = preload("res://brain_visualizer_source/destination.material")
+@onready var glow = preload("res://Feagi-Godot-Interface/UI/Brain_Visualizer/assets/glow.material")
+@onready var destination = preload("res://Feagi-Godot-Interface/UI/Brain_Visualizer/assets/destination.material")
 @onready var red = preload("res://Feagi-Godot-Interface/UI/Brain_Visualizer/assets/red.material")
