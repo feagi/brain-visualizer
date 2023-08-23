48de6dbe941e8ad638db4f3fad8ebebd7cafb872
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-23 16:38:02 -0400

    Added Float Input

--- godot_source/Feagi-Godot-Interface/UI/Windows/Elements/TextBoxes/FloatInput.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Elements/TextBoxes/FloatInput.gd
@@ -1,24 +1,32 @@
 extends LineEdit
-class_name TextInput
-## Text box that user can input Strings into
+class_name FloatInput
+## Text Box that use can input floats into
 
 # useful properties inherited
-# text (NOTE - changing this via code does not cause signal up!)
 # editable
 # max_length
 
 # do not use the text_changed and text_submitted signals due top various limitations with them, unless you have a specific reason to
 
 ## Only emits if user changes the text THEN focuses off the textbox
-signal text_confirmed(new_text: String)
+signal float_confirmed(new_float: float)
 
 ## If signaling up via 'text_confirmed' should be enabled. Does nothing after '_ready'
 @export var enable_signaling_on_ready: bool = true
-var _previous_text: String
+## The float to start with
+@export var initial_float: float = 0.0
+## what to add before the float
+@export var prefix: String = ""
+## what to add after the float
+@export var suffix: String = ""
+
+var _previous_float: float
 
 func _ready():
-	text = _previous_text
+	_previous_float = initial_float
+	_set_visible_text(initial_float)
 	toggle_signaling_up(enable_signaling_on_ready)
+	focus_entered.connect(_on_focus)
 
 ## Toggles signaling if the internal value changed, similar to setting 'editable' but without UI changes
 func toggle_signaling_up(enable: bool) -> void:
@@ -29,9 +37,20 @@ func toggle_signaling_up(enable: bool) -> void:
 	if !is_connected("focus_exited", _emit_if_text_changed): return # do not disconnect twice!
 	focus_exited.disconnect(_emit_if_text_changed)
 	return
-	
+
+## used so user doesnt have to get rid of prefix and suffix when typing
+func _on_focus():
+	text = str(_previous_float)
+
 func _emit_if_text_changed() -> void:
-	if text == _previous_text:
+	if !text.is_valid_float():
+		_set_visible_text(_previous_float)
 		return
-	_previous_text = text
-	text_confirmed.emit(text)
+	if text.to_float() == _previous_float:
+		return
+	_previous_float = text.to_float()
+	float_confirmed.emit(_previous_float)
+	_set_visible_text(_previous_float)
+
+func _set_visible_text(new_float: float) -> void:
+	text = prefix + str(new_float) + suffix
