ecfb0ba8b884023f09bc5862bea00cd20d7d3c0e
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-28 12:08:16 -0400

    Finished LB Top

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Left_Bar/Parts/LeftBarTop.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Left_Bar/Parts/LeftBarTop.gd
@@ -1,6 +1,10 @@
 extends VBoxContainer
 class_name LeftBarTop
 ## Top Section of the Left Bar Window
+## TODO add field color changing
+
+## User pressed update button, the following changes are requested
+signal user_requested_update(changed_values: Dictionary)
 
 var cortical_name: StringName:
 	set(v):
@@ -22,7 +26,8 @@ var _line_cortical_ID: TextInput
 var _line_cortical_type: TextInput
 var _vector_position: Vector3iField
 var _vector_dimensions: Vector3iField
-
+var _hiding_container: HiderFrozenSize
+var _growing_cortical_update: Dictionary
 
 func _ready():
 	super._ready()
@@ -31,9 +36,33 @@ func _ready():
 	_line_cortical_type = $Row_Cortical_Type/Cortical_Type
 	_vector_position = $Cortical_Position
 	_vector_dimensions = $Cortical_Size
+	_hiding_container = $Update_Button_Hider
 
-func _user_edit_3D_position(new_position: Vector3i):
-	user_requested_3D_position.emit(new_position)
+	_line_cortical_name.text_confirmed.connect(_user_edit_name)
+	_vector_position.user_updated_vector.connect(_user_edit_3D_position)
+	_vector_dimensions.user_updated_vector.connect(_user_edit_dimension)
+	var update_button: TextButton_Element = _hiding_container.get_node("Update_Button")
+	update_button.pressed.connect(_user_requests_update)
 
-func _user_edit_dimension(new_dimension: Vector3i):
-	user_requested_dimension.emit(new_dimension)
+
+## FEAGI confirmed changes, show this in the UI and clear the backend dict
+func FEAGI_confirmed_update() -> void:
+	_growing_cortical_update = {}
+	_hiding_container.toggle_child_visibility(false)
+	# TODO change edited color of fields
+
+## User pressed update button
+func _user_requests_update() -> void:
+	user_requested_update.emit(_growing_cortical_update)
+
+func _user_edit_name(new_name: String) -> void:
+	_growing_cortical_update["cortical_name"] = new_name
+	_hiding_container.toggle_child_visibility(true)
+
+func _user_edit_3D_position(new_position: Vector3i) -> void:
+	_growing_cortical_update["cortical_coordinates"] = str(new_position)
+	_hiding_container.toggle_child_visibility(true)
+
+func _user_edit_dimension(new_dimension: Vector3i) -> void:
+	_growing_cortical_update["cortical_dimensions"] = str(new_dimension)
+	_hiding_container.toggle_child_visibility(true)
