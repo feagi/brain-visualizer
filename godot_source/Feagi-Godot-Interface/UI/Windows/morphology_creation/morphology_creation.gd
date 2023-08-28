9c293ab049badb8d63422c101433bed6dccd95a1
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-28 17:08:47 -0400

    added gap, composite

--- godot_source/Feagi-Godot-Interface/UI/Windows/morphology_creation/morphology_creation.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/morphology_creation/morphology_creation.gd
@@ -1,5 +1,5 @@
 extends Panel
-
+var gap: int = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,4 +7,7 @@ func _ready():
 		var new_node = $Patterns_BOX/Scroll_Vertical/VBoxContainer/HBoxContainer.duplicate()
 		$Patterns_BOX/Scroll_Vertical/VBoxContainer.add_child(new_node)
 		## Only to test if it does what it should be. Don't forget to delete this
-		
+	for i in $RadioButtons.get_children():
+		if i.get_class() == "TextureButton":
+			i.custom_minimum_size.x = gap
+			
