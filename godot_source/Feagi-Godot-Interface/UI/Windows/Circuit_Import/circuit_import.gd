9ba406502bed836ba688881db18fff3872262cf8
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-29 15:27:08 -0400

    added signal from UI to send name selected to FEAGI thru api

--- godot_source/Feagi-Godot-Interface/UI/Windows/Circuit_Import/circuit_import.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Circuit_Import/circuit_import.gd
@@ -6,10 +6,23 @@ func _ready():
 	$HBoxContainer3/Vector3Spinbox/W.editable = false
 	$HBoxContainer3/Vector3Spinbox/H.editable = false
 	$HBoxContainer3/Vector3Spinbox/D.editable = false
-
+	$TextEdit.editable = false
+	FeagiCacheEvents.available_circuit_listing_updated.connect(update_circuit)
 
 func _on_nc_button_pressed():
+	size.x = 329
 	if visible:
 		visible = false
 	else:
 		visible = true
+		$HBoxContainer/OptionButton.clear()
+		$HBoxContainer/OptionButton.add_item("")
+		FeagiRequests.refresh_available_circuits()
+
+func update_circuit(data):
+	for i in data:
+		$HBoxContainer/OptionButton.add_item(i)
+	size.x += $HBoxContainer/OptionButton.size.x + 65
+
+func _on_option_button_item_selected(index):
+	FeagiRequests.get_circuit_size($HBoxContainer/OptionButton.text)
