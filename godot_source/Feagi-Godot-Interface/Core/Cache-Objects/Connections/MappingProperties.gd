4d859672fb78e71f3c5728137d32d42fa9755774
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-08-29 13:05:46 -0400

    Added MappingProperties to defiine  all mappings between 2 cortical areas

---
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Connections/MappingProperties.gd
@@ -0,0 +1,25 @@
+extends Object
+class_name MappingProperties
+## Holds all [MappingProperty] objects / data relevant between a source and destination cortical area
+## This object is not stored in the cache, it is created as needed, then can be discarded
+
+var source_cortical_area: CorticalArea:
+    get: return _src_cortical
+var destination_cortical_area: CorticalArea:
+    get: return _dst_cortical
+var mappings: Array[MappingProperty]
+
+var _src_cortical: CorticalArea
+var _dst_cortical: CorticalArea
+
+func _init(source_area: CorticalArea, destination_area: CorticalArea, mappings_between_them: Array[MappingProperty]) -> void:
+    _src_cortical = source_area
+    _dst_cortical = destination_area
+    mappings = mappings_between_them
+
+## Returns an array of the internal [MappingProperty] objects as FEAGI formatted dictionaries
+func to_array() -> Array[Dictionary]:
+    var output: Array[Dictionary] = []
+    for mapping in mappings:
+        output.append(mapping)
+    return output
--- godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Connections/MappingProperty.gd
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Connections/MappingProperty.gd
@@ -1,14 +1,10 @@
 extends Object
 class_name MappingProperty
-## Mapping property between 2 cortical areas
+## a SINGLE Mapping property between 2 cortical areas (there can be multiple, which is stored in [MappingProperties])
 ## This object is never actually stored in a cache, its created as needed to represent a connections details
 
 const INT8_MAX: int = 2147483647 # This is likely the max supported int in vector3i, but you should NOT be getting this close anyways
 
-var source_cortical_area: CorticalArea:
-    get: return _src_cortical_area
-var destination_cortical_area: CorticalArea:
-    get: return _dst_cortical_area
 var morphology_used: Morphology:
     get: return _morphology_used
 var is_plastic: bool:
@@ -21,13 +17,18 @@ var _morphology_used: Morphology
 var _scalar: Vector3i # must all be non-zero positive
 var _post_synaptic_current_multiplier: float
 var _plasticity_flag: bool
-var _src_cortical_area: CorticalArea
-var _dst_cortical_area: CorticalArea
 
-func _init(source_cortical: CorticalArea, destination_cortical: CorticalArea, morphology: Morphology, positive_scalar: Vector3i, current_multilpier: float, plasticity: bool):
-    _src_cortical_area = source_cortical
-    _dst_cortical_area = destination_cortical
+func _init(morphology: Morphology, positive_scalar: Vector3i, current_multilpier: float, plasticity: bool):
     _morphology_used = morphology
     _scalar = Vector3i(FEAGIUtils.bounds_int(positive_scalar.x, 1, INT8_MAX), FEAGIUtils.bounds_int(positive_scalar.y, 1, INT8_MAX), FEAGIUtils.bounds_int(positive_scalar.z, 1, INT8_MAX))
     _post_synaptic_current_multiplier = current_multilpier
     _plasticity_flag = plasticity
+
+## Returns a dictionary of this object in the same format FEAGI expects
+func to_dictionary() -> Dictionary:
+    return {
+        "morphology_id": _morphology_used.name,
+        "morphology_scalar": FEAGIUtils.vector3i_to_array(_scalar),
+        "postSynapticCurrent_multiplier": _post_synaptic_current_multiplier,
+        "plasticity_flag": _plasticity_flag
+    }
