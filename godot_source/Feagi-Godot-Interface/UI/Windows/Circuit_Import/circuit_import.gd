be8d7bc2e0764aa3164142edc33a423dfca2205a
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-29 14:10:07 -0400

    editable disabled on WDH

--- godot_source/Feagi-Godot-Interface/UI/Windows/Circuit_Import/circuit_import.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Circuit_Import/circuit_import.gd
@@ -3,12 +3,9 @@ extends Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	$HBoxContainer3/Vector3Spinbox/W.editable = false
+	$HBoxContainer3/Vector3Spinbox/H.editable = false
+	$HBoxContainer3/Vector3Spinbox/D.editable = false
 
 
 func _on_nc_button_pressed():
