048a3f1dd3c748fcf9243851d028b2a2bd1c3e83
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-29 12:42:10 -0400

    clean up

--- godot_source/Feagi-Godot-Interface/UI/Windows/Create_cortical/create_cortical.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Create_cortical/create_cortical.gd
@@ -37,18 +37,6 @@ func _on_button_2_pressed():
 	$"../../Brain_Visualizer".delete_example()
 	#var json_data: Dictionary = {}
 	if $VBoxContainer/BoxContainer/RadioButtons/CUSTOM.is_pressed():
-		#json_data["cortical_type"] = "CUSTOM"
-		#json_data["cortical_name"] = $VBoxContainer/BoxContainer2/LineEdit.text
-		#json_data["coordinates_3d"] = []
-		#json_data["cortical_dimensions"] = []
-		#json_data["coordinates_3d"].append($VBoxContainer/XYZ/IntX.value)
-		#json_data["coordinates_3d"].append($VBoxContainer/XYZ/IntY.value)
-		#json_data["coordinates_3d"].append($VBoxContainer/XYZ/IntZ.value)
-		#json_data["cortical_dimensions"].append($VBoxContainer/WDH/W.value)
-		#json_data["cortical_dimensions"].append($VBoxContainer/WDH/H.value)
-		#json_data["cortical_dimensions"].append($VBoxContainer/WDH/D.value)
-
-#		generate_single_cortical(json_data["coordinates_3d"][0], json_data["coordinates_3d"][1], json_data["coordinates_3d"][2], json_data["cortical_dimensions"][0], json_data["cortical_dimensions"][1], json_data["cortical_dimensions"][2], json_data["cortical_name"])
 		FeagiRequests.add_custom_cortical_area(
 			$VBoxContainer/BoxContainer2/LineEdit.text,
 			Vector3($VBoxContainer/XYZ/IntX.value, $VBoxContainer/XYZ/IntY.value, $VBoxContainer/XYZ/IntZ.value),
@@ -57,8 +45,6 @@ func _on_button_2_pressed():
 			Vector2i(0,0),
 			CorticalArea.CORTICAL_AREA_TYPE.CUSTOM
 		)
-		print($VBoxContainer/BoxContainer2/LineEdit.text, Vector3($VBoxContainer/XYZ/IntX.value, $VBoxContainer/XYZ/IntY.value, $VBoxContainer/XYZ/IntZ.value),
-			Vector3($VBoxContainer/WDH/W.value, $VBoxContainer/WDH/H.value, $VBoxContainer/WDH/D.value))
 		$Button2.release_focus()
 	$VBoxContainer/XYZ/IntX.value = 0
 	$VBoxContainer/XYZ/IntY.value = 0
