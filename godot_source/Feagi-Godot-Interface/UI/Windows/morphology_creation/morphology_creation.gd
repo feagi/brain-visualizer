a470b27fb28b638244446a9a9331c2981687fe7f
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-30 17:42:29 -0400

    duplication work begin in vectors

--- godot_source/Feagi-Godot-Interface/UI/Windows/morphology_creation/morphology_creation.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/morphology_creation/morphology_creation.gd
@@ -1,12 +1,10 @@
 extends Panel
 var gap: int = 20
+var duplicated_reference_name_list = [] # store all node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in 5:
-		var new_node = $Patterns_BOX/Scroll_Vertical/VBoxContainer/HBoxContainer.duplicate()
-		$Patterns_BOX/Scroll_Vertical/VBoxContainer.add_child(new_node)
-		## Only to test if it does what it should be. Don't forget to delete this
+	## Only to test if it does what it should be. Don't forget to delete this
 	for i in $RadioButtons.get_children():
 		if i.get_class() == "TextureButton":
 			i.custom_minimum_size.x = gap
@@ -15,6 +13,7 @@ func _on_composite_pressed():
 	$Composite_BOX.visible = true
 	$Vector_BOX.visible = false
 	$Patterns_BOX.visible = false
+	$add_row.visible= false
 
 
 
@@ -22,12 +21,15 @@ func _on_vectors_pressed():
 	$Composite_BOX.visible = false
 	$Vector_BOX.visible = true
 	$Patterns_BOX.visible = false
+	$add_row.visible= true
 
 
 func _on_patterns_pressed():
 	$Composite_BOX.visible = false
 	$Vector_BOX.visible = false
 	$Patterns_BOX.visible = true
+	_on_add_row_pressed()
+	$add_row.visible= true
 
 
 func _on_nm_add_button_pressed():
@@ -49,3 +51,13 @@ func _on_visibility_changed():
 		$Composite_BOX.set_press(false)
 		$Vector_BOX.set_press(false)
 		$Patterns_BOX.set_press(false)
+
+
+func _on_add_row_pressed():
+	if $Vector_BOX.visible:
+		var new_node = $Vector_BOX/Scroll_Vertical/VBoxContainer/XYZ_BOX.duplicate()
+		new_node.visible = true
+		$Vector_BOX/Scroll_Vertical/VBoxContainer.add_child(new_node)
+		duplicated_reference_name_list.append(new_node)
+	elif $Patterns_BOX.visible:
+		print("patterns")
