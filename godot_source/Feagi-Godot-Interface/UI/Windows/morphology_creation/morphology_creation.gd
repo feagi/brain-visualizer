b0a274a3f612f0b9427d9f0c3c11ca081a0b7cc2
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-30 19:22:02 -0400

    added clea nodes, delete node and list

--- godot_source/Feagi-Godot-Interface/UI/Windows/morphology_creation/morphology_creation.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/morphology_creation/morphology_creation.gd
@@ -1,6 +1,7 @@
 extends Panel
 var gap: int = 20
-var duplicated_reference_name_list = [] # store all node
+var duplicated_reference_name_listV = [] # store all node in Vectors
+var duplicated_reference_name_listP = [] # Store all node in Patterns
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,14 +23,17 @@ func _on_vectors_pressed():
 	$Vector_BOX.visible = true
 	$Patterns_BOX.visible = false
 	$add_row.visible= true
+	if not duplicated_reference_name_listV:
+		_on_add_row_pressed() # directly ping the signal function
 
 
 func _on_patterns_pressed():
 	$Composite_BOX.visible = false
 	$Vector_BOX.visible = false
 	$Patterns_BOX.visible = true
-	_on_add_row_pressed()
 	$add_row.visible= true
+	if not duplicated_reference_name_listP:
+		_on_add_row_pressed() # directly ping the signal function
 
 
 func _on_nm_add_button_pressed():
@@ -42,22 +46,54 @@ func _on_nm_add_button_pressed():
 		for i in FeagiCache.morphology_cache.available_morphologies:
 			$Composite_BOX/VBoxContainer/HBoxContainer3/OptionButton.add_item(i)
 
+func clear_list_of_pattern():
+	print("heree")
+	for i in duplicated_reference_name_listP:
+		i.queue_free()
+	duplicated_reference_name_listP = []
 
+func clear_list_of_vector():
+	for i in duplicated_reference_name_listV:
+		i.queue_free()
+	duplicated_reference_name_listV = []
+	
 func _on_visibility_changed():
 	if not visible:
+		clear_list_of_pattern()
+		clear_list_of_vector()
 		$Composite_BOX.visible = false
 		$Vector_BOX.visible = false
 		$Patterns_BOX.visible = false
-		$Composite_BOX.set_press(false)
-		$Vector_BOX.set_press(false)
-		$Patterns_BOX.set_press(false)
+		$RadioButtons/composite.button_pressed = false
+		$RadioButtons/vectors.button_pressed = false
+		$RadioButtons/patterns.button_pressed = false
 
+func delete_itself(node):
+	for i in duplicated_reference_name_listP:
+		if node == i:
+			duplicated_reference_name_listP.erase(i)
+			node.queue_free()
+			return
+	for i in duplicated_reference_name_listV:
+		if node == i:
+			duplicated_reference_name_listV.erase(i)
+			node.queue_free()
+			return
 
 func _on_add_row_pressed():
 	if $Vector_BOX.visible:
 		var new_node = $Vector_BOX/Scroll_Vertical/VBoxContainer/XYZ_BOX.duplicate()
 		new_node.visible = true
+		new_node.get_node("Button").connect("pressed",Callable(delete_itself).bind(new_node))
 		$Vector_BOX/Scroll_Vertical/VBoxContainer.add_child(new_node)
-		duplicated_reference_name_list.append(new_node)
+		duplicated_reference_name_listV.append(new_node)
 	elif $Patterns_BOX.visible:
-		print("patterns")
+		var new_node = $Patterns_BOX/Scroll_Vertical/VBoxContainer/HBoxContainer.duplicate()
+		new_node.visible = true
+		new_node.get_node("remove").connect("pressed",Callable(delete_itself).bind(new_node))
+		$Patterns_BOX/Scroll_Vertical/VBoxContainer.add_child(new_node)
+		duplicated_reference_name_listP.append(new_node)
+
+
+func _on_create_pressed():
+	pass # Replace with function body.
