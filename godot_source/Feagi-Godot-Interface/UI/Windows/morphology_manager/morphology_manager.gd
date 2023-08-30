98eb2fe6594ee7832bbba063782d98ef48d3bacf
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-30 14:50:27 -0400

    delete unnecessary function

--- godot_source/Feagi-Godot-Interface/UI/Windows/morphology_manager/morphology_manager.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/morphology_manager/morphology_manager.gd
@@ -1,7 +1,1 @@
 extends Panel
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$Scroll_Vertical.prefab_to_spawn = preload("res://Feagi-Godot-Interface/UI/Windows/Small_Groupings/spawn_prefabs/Button_Prefab/ScrollButtonPrefab.tscn")
-	$Scroll_Vertical.spawn_list_item({"name":"test2", "text":"test4"})
