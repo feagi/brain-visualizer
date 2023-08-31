97d0254e8204f726a144228c74a0409c4ffb7a60
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-31 18:22:13 -0400

    add non-image and image

--- godot_source/Feagi-Godot-Interface/UI/Windows/Quick_Connect/morphology_menulist.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Quick_Connect/morphology_menulist.gd
@@ -1,14 +1,17 @@
-extends ItemList
+extends Panel
 
+var stored_name: Array = []
+var node_list: Array = [] # Just to store node's reference so I can clear them
 
-func _ready():
-	dir_contents("res://Feagi-Godot-Interface/UI/Resources/morphology_icons/")
+"""
+Needs to add singal to prevent the every loop 
+"""
 
-	
-
-func dir_contents(path):
+func dir_contents(path): # res://Feagi-Godot-Interface/UI/Resources/morphology_icons/
+	for i in node_list:
+		i.queue_free()
+	node_list = []
 	var dir = DirAccess.open(path)
-	var counter = 0
 	if dir:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
@@ -26,17 +29,25 @@ func dir_contents(path):
 						# Remove .import and .png extensions from the name
 						var clean_name = file_name.replace(".import", "").replace(".png", "")
 						var path_to_icon = "res://Feagi-Godot-Interface/UI/Resources/morphology_icons/"+str(file_name)
-#						var icon_texture = ImageTexture.new()
-#						var image = Image.load(path_to_icon)                
-#						# Resize the image
-#						var new_size = Vector2(50, 50)  # Adjust the size as needed
-#						image.resize(new_size)
-						add_item(clean_name, load(path_to_icon), true)
-						# Pause  this, we will revisit this
-						counter += 1
+						if clean_name in FeagiCache.morphology_cache.available_morphologies:
+							var new_node = $Scroll_Vertical/VBoxContainer/TextureButton.duplicate()
+							$Scroll_Vertical/VBoxContainer.add_child(new_node)
+							new_node.texture_normal = load(path_to_icon)
+							new_node.visible = true
+							new_node.set_name(clean_name)
+							new_node.connect("pressed", Callable($".."._on_texture_button_pressed).bind(new_node.get_name()))
+							node_list.append(new_node)
+							stored_name.append(clean_name)
 					else:
 						print("Found other file: " + file_name)
 			file_name = dir.get_next()
 	else:
 		print("An error occurred when trying to access the path.")
-
+	for i in FeagiCache.morphology_cache.available_morphologies:
+		if not i in stored_name:
+			var new_node = $Scroll_Vertical/VBoxContainer/Button_morp.duplicate()
+			new_node.visible = true
+			new_node.text = i
+			new_node.connect("pressed", Callable($".."._on_texture_button_pressed).bind(new_node.text))
+			node_list.append(new_node)
+			$Scroll_Vertical/VBoxContainer.add_child(new_node)
