59d6d96d6dc3d0c56a6069dc9d04a3fce4a22112
kakcalu13 <kevin.a.araujo@gmail.com>
2023-08-31 18:21:07 -0400

    fixed cortical list

--- godot_source/Feagi-Godot-Interface/UI/Windows/Cortical_List/cortical_list.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Cortical_List/cortical_list.gd
@@ -5,6 +5,6 @@ func _on_ca_view_button_pressed():
 		visible = false
 	else:
 		visible = true
+		clear() # we need to fix this without clear. 
 		for i in FeagiCache.cortical_areas_cache.cortical_areas:
-			clear() # we need to fix this without clear. 
 			add_item(FeagiCache.cortical_areas_cache.cortical_areas[i].name)
