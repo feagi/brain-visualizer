992e035128f2b309be009f07a6bd24dc222bbb33
kakcalu13 <kevin.a.araujo@gmail.com>
2023-09-05 10:52:28 -0400

    add morphology manager

--- godot_source/Feagi-Godot-Interface/UI/Windows/morphology_manager/morphology_manager.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/morphology_manager/morphology_manager.gd
@@ -1,5 +1,6 @@
 extends Panel
 
+var morphology_list = []
 
 func ready():
 	pass
@@ -9,3 +10,14 @@ func _on_nm_settings_button_pressed():
 		visible = false
 	else:
 		visible = true
+		morphology_list = []
+		for i in FeagiCache.morphology_cache.available_morphologies:
+			morphology_list.append(i)
+			var new_node = $Scroll_Vertical/VBoxContainer/morphology_button.duplicate()
+			new_node.text = i
+			new_node.visible = true
+			new_node.connect("pressed", Callable(_on_texture_button_pressed).bind(new_node.text))
+			$Scroll_Vertical/VBoxContainer.add_child(new_node)
+
+func _on_texture_button_pressed(value):
+	print(value) # Needs to use API to send data
