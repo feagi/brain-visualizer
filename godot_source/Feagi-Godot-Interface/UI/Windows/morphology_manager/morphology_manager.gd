1fabae24f42ca15a1b90c170e86f1626f66e3b44
kakcalu13 <kevin.a.araujo@gmail.com>
2023-09-05 13:41:50 -0400

    push latest

--- godot_source/Feagi-Godot-Interface/UI/Windows/morphology_manager/morphology_manager.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/morphology_manager/morphology_manager.gd
@@ -1,9 +1,11 @@
 extends Panel
 
 var morphology_list = []
+var name_file = []
 
 func ready():
-	pass
+	FeagiCacheEvents.morphology_cache.mor
+	FeagiCacheEvents.morphology_cache.morphology_updated(updated_morphology_list)
 
 func _on_nm_settings_button_pressed():
 	if visible:
@@ -20,4 +22,32 @@ func _on_nm_settings_button_pressed():
 			$Scroll_Vertical/VBoxContainer.add_child(new_node)
 
 func _on_texture_button_pressed(value):
-	print(value) # Needs to use API to send data
+	pass
+
+func updated_morphology_list():
+	print("worked")
+	var dir = DirAccess.open("res://Feagi-Godot-Interface/UI/Resources/morphology_icons/")
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				# Check if the file extension is .import
+				if file_name.ends_with(".import"):
+					# Ignore .import files
+					pass
+				else:
+					# Check if the file extension is .png
+					if file_name.ends_with(".png"):
+						# Remove .import and .png extensions from the name
+						var clean_name = file_name.replace(".import", "").replace(".png", "")
+						if clean_name in FeagiCache.morphology_cache.available_morphologies:
+							name_file.append(clean_name)
+					else:
+						print("Found other file: " + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	print("HERE: ", name_file)
