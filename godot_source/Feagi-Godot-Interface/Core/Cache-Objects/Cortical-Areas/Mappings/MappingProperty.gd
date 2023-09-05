ca725ff4124f80847c75018551cc86a775c867c4
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-09-05 16:03:40 -0400

    Added new convars to mappings, moved factory as a static funciton to the mappingProperty object

--- godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Cortical-Areas/Mappings/MappingProperty.gd
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Cortical-Areas/Mappings/MappingProperty.gd
@@ -7,18 +7,30 @@ const INT8_MAX: int = 2147483647 # This is likely the max supported int in vecto
 
 var morphology_used: Morphology:
     get: return _morphology_used
+var scalar: Vector3i:
+    get: return _scalar
 var is_plastic: bool:
     get: return _plasticity_flag
 var post_synaptic_current_multiplier: float:
     get: return post_synaptic_current_multiplier
+var plasticity_multiplier: float:
+    get: return _plasticity_multiplier
+var LTP_multiplier: float:
+    get: return _LTP_multiplier
+var LTD_multiplier: float:
+    get: return _LTD_multiplier
 
 
 var _morphology_used: Morphology
 var _scalar: Vector3i # must all be non-zero positive
 var _post_synaptic_current_multiplier: float
 var _plasticity_flag: bool
+var _plasticity_multiplier: float
+var _LTP_multiplier: float
+var _LTD_multiplier: float
 
-func _init(morphology: Morphology, positive_scalar: Vector3i, current_multilpier: float, plasticity: bool):
+
+func _init(morphology: Morphology, positive_scalar: Vector3i, current_multilpier: float, plasticity: bool, plasticity_multiplier: float = 1.0, LTP_multiplier: float = 1.0, LTD_multiplier: float = 1.0):
     _morphology_used = morphology
     _scalar = Vector3i(FEAGIUtils.bounds_int(positive_scalar.x, 1, INT8_MAX), FEAGIUtils.bounds_int(positive_scalar.y, 1, INT8_MAX), FEAGIUtils.bounds_int(positive_scalar.z, 1, INT8_MAX))
     _post_synaptic_current_multiplier = current_multilpier
@@ -26,9 +38,34 @@ func _init(morphology: Morphology, positive_scalar: Vector3i, current_multilpier
 
 ## Returns a dictionary of this object in the same format FEAGI expects
 func to_dictionary() -> Dictionary:
-    return {
-        "morphology_id": _morphology_used.name,
-        "morphology_scalar": FEAGIUtils.vector3i_to_array(_scalar),
-        "postSynapticCurrent_multiplier": _post_synaptic_current_multiplier,
-        "plasticity_flag": _plasticity_flag
-    }
+    if !_plasticity_flag:
+        return {
+            "morphology_id": _morphology_used.name,
+            "morphology_scalar": FEAGIUtils.vector3i_to_array(_scalar),
+            "postSynapticCurrent_multiplier": _post_synaptic_current_multiplier,
+            "plasticity_flag": _plasticity_flag,
+        }
+    else:
+        return {
+            "morphology_id": _morphology_used.name,
+            "morphology_scalar": FEAGIUtils.vector3i_to_array(_scalar),
+            "postSynapticCurrent_multiplier": _post_synaptic_current_multiplier,
+            "plasticity_flag": _plasticity_flag,
+            "plasticity_multiplier": _plasticity_multiplier,
+            "ltp_multiplier": _LTP_multiplier,
+            "ltd_multiplier": _LTD_multiplier,
+        }
+
+## Given the dictionary from FEAGI directly creates a MappingProperty object
+static func from_dict(mapping_property: Dictionary) -> MappingProperty:
+    var morphology_cached: Morphology = FeagiCache.morphology_cache.available_morphologies[mapping_property["morphology_id"]]
+    var scalar_used: Vector3i = FEAGIUtils.array_to_vector3i(mapping_property["morphology_scalar"])
+    var psp_multiplier: float = mapping_property["postSynapticCurrent_multiplier"]
+    var plasticity: bool = mapping_property["plasticity_flag"]
+    if !plasticity:
+        return MappingProperty.new(morphology_cached, scalar_used, psp_multiplier, plasticity)
+    else:
+        var plasticity_multiplier_used: float = mapping_property["plasticity_multiplier"]
+        var LTP_multiplier_used: float = mapping_property["ltp_multiplier"]
+        var LTD_multiplier_used: float = mapping_property["ltd_multiplier"]
+        return MappingProperty.new(morphology_cached, scalar_used, psp_multiplier, plasticity, plasticity_multiplier_used, LTP_multiplier_used, LTD_multiplier_used)
