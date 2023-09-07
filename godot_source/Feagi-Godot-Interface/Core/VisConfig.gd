5475de56e4e7267199c50ef1d322c3285fd8268c
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-09-07 13:21:24 -0400

    Guard claused for the camera! No longer moves when we are dragging a window or typing (to be done)

--- godot_source/Feagi-Godot-Interface/Core/VisConfig.gd
+++ godot_source/Feagi-Godot-Interface/Core/VisConfig.gd
@@ -4,6 +4,8 @@ extends Node
 
 signal screen_size_changed(new_screen_size: Vector2)
 signal UI_settings_changed()
+signal user_changed_typing_status(is_typing: bool) ## Emits whenever a user starts / stops interaction with a text box
+signal user_changed_window_drag_status(is_dragging_a_window: bool) ## Emits whenever a user starts / stops dragging a window
 
 var screen_size: Vector2:  # keep as float for easy division
 	get: return _screen_size
@@ -17,12 +19,27 @@ var minimum_button_size_pixel: Vector2i:
 ## Reference to the window manager
 var window_manager: WindowManager # set from the window manager itself when it is ready, otherwise this is null!\
 
-
 var is_premium: bool = true
 var left_bar_allow_premium_monitoring: bool = true # todo: should probably tie in with above
 
+## Is the user currently typing in a textbox somewhere
+var is_user_typing: bool:
+	get: return _is_user_typing
+	set(v):
+		_is_user_typing = v
+		user_changed_typing_status.emit(v)
+
+## Is the user currently dragging a window somewhere
+var is_user_dragging_a_window: bool:
+	get: return _is_user_dragging_a_window
+	set(v):
+		_is_user_dragging_a_window = v
+		user_changed_window_drag_status.emit(v)
+
 var _screen_size: Vector2
 var _minimum_button_size_pixel: Vector2i = Vector2i(40,40) # HINT: number should be divisible by 4
+var _is_user_typing: bool = false
+var _is_user_dragging_a_window: bool = false
 
 func _ready():
 	_screen_size = get_viewport().get_visible_rect().size
