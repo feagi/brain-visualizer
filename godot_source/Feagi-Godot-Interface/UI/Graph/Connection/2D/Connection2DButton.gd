8768feb95e35a30595073f04d0b6f46344000090
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-09-14 17:48:22 -0400

    tab/space sillyness

--- godot_source/Feagi-Godot-Interface/UI/Graph/Connection/2D/Connection2DButton.gd
+++ godot_source/Feagi-Godot-Interface/UI/Graph/Connection/2D/Connection2DButton.gd
@@ -3,10 +3,10 @@ class_name Connection2DButton
 ## Shows number of mappings
 
 func _init(number_mappings: int):
-    update_mapping_counter(number_mappings)
+	update_mapping_counter(number_mappings)
 
 func update_mapping_counter(number_of_mappings: int):
-    text = str(number_of_mappings)
+	text = str(number_of_mappings)
 
 func update_position(midpoint: Vector2) -> void:
-    position = midpoint - (size / 2.0)
+	position = midpoint - (size / 2.0)
