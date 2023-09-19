7bbe95b83890308a67acaa6f6b7d61ab3c0e23d2
kakcalu13 <kevin.a.araujo@gmail.com>
2023-09-19 14:31:12 -0400

    add 3d indicator

---
+++ godot_source/Feagi-Godot-Interface/UI/Resources/Shared_components/3d_indicator.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+var flag_once = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+		$Cube.set_surface_override_material(0, global_material.glow)
+		$Cube001.set_surface_override_material(0, global_material.glow)
+		$Cube002.set_surface_override_material(0, global_material.glow)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if network_setting.state == 1:
+		if flag_once:
+			Autoload_variable.feagi_flag = true
+			flag_once = false
+		$Cube.set_surface_override_material(0, global_material.red)
+		$Cube001.set_surface_override_material(0, global_material.red)
+		$Cube002.set_surface_override_material(0, global_material.red)
+	else:
+		$Cube.set_surface_override_material(0, global_material.glow)
+		$Cube001.set_surface_override_material(0, global_material.glow)
+		$Cube002.set_surface_override_material(0, global_material.glow)
