6a41b8ec959762fd9fdcc10d3e2eb9f696728d15
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-09-21 12:12:32 -0400

    Create Container Shrinker to aid in shrinking windows

---
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/ContainerShrinker.gd
@@ -0,0 +1,9 @@
+extends BoxContainer
+class_name ContainerShrinker
+## Holds input function that triggers a size recalculation for all contained
+
+## Any events that should cause a size recalculation should be connected via signal here
+func recalculate_size() -> void:
+	size = Vector2(0,0) # This is terrible. Too bad!
+	
+	
