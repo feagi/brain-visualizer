103bb00dc23468ee44066558049f4dfe6a57162c
kakcalu13 <kevin.a.araujo@gmail.com>
2023-09-21 16:12:04 -0400

    remove display message

--- godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/timer.gd
+++ godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/timer.gd
@@ -4,15 +4,18 @@ var start_timer
 var array_delta =[]
 
 func _ready():
-	while len(array_delta) < 6:
-		await get_tree().create_timer(60.0).timeout
-		start_timer = Time.get_ticks_msec()
-		$"../../../FEAGIInterface".net.websocket_send("ping")
-	var final_delta = 0
-	for i in array_delta:
-		final_delta += i
-	final_delta = final_delta/6
-	$"../3d_indicator/Sprite3D/SubViewport/delta_label".text = str(final_delta) + " ms"
+	while true:
+		while len(array_delta) < 6:
+			start_timer = Time.get_ticks_msec()
+			$"../../../FEAGIInterface".net.websocket_send("ping")
+			await get_tree().create_timer(1.0).timeout
+		var final_delta = 0
+		for i in array_delta:
+			final_delta += i
+		final_delta = final_delta/6
+		$"../3d_indicator/Sprite3D/SubViewport/delta_label".text = str(final_delta) + " ms"
+		array_delta =[]
+		await get_tree().create_timer(5).timeout
 	
 
 func latency_calculation(end):
