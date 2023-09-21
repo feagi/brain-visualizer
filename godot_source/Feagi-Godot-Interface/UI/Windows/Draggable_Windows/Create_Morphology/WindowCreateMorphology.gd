089deeaaabe8c5f15445a846fbbc890eeb51d6d7
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-09-21 13:34:24 -0400

    Fixed TitleBar

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Create_Morphology/WindowCreateMorphology.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Create_Morphology/WindowCreateMorphology.gd
@@ -23,9 +23,7 @@ func _ready():
 	_composite = $Container/ElementMorphologyCompositeView
 	_vectors = $Container/ElementMorphologyVectorsView
 	_patterns = $Container/ElementMorphologyPatternView
-	
 
-	_patterns_vector_list = $Container/Patterns/Patterns/BoxContainer
 
 	_composite.visible = false
 	_vectors.visible = false
