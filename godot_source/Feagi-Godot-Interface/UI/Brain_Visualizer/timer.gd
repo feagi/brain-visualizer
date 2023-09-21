6fcbef96fbcb1ac07646153ef91ecd566de9df34
kakcalu13 <kevin.a.araujo@gmail.com>
2023-09-21 17:21:21 -0400

    remove nodes and move the script to attach the latency lineedit

--- godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/timer.gd
+++ godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/timer.gd
@@ -1,24 +1,26 @@
-extends Node3D
+extends LineEdit
 
 var start_timer
 var array_delta =[]
 
+
 func _ready():
+	FeagiEvents.retrieved_latest_ping.connect(latency_calculation)
 	while true:
 		while len(array_delta) < 6:
 			start_timer = Time.get_ticks_msec()
-			$"../../../FEAGIInterface".net.websocket_send("ping")
+			$"../../../../../FEAGIInterface".net.websocket_send("ping")
 			await get_tree().create_timer(1.0).timeout
 		var final_delta = 0
 		for i in array_delta:
 			final_delta += i
 		final_delta = final_delta/6
-		$"../3d_indicator/Sprite3D/SubViewport/delta_label".text = str(final_delta) + " ms"
+		text = str(final_delta) + " ms"
 		array_delta =[]
 		await get_tree().create_timer(5).timeout
-	
+
 
 func latency_calculation(end):
 	var delta_timer = (end - start_timer)/2
-	$"../3d_indicator/Sprite3D/SubViewport/delta_label".text = str(delta_timer) + " ms"
+	text = str(delta_timer) + " ms"
 	array_delta.append(delta_timer)
