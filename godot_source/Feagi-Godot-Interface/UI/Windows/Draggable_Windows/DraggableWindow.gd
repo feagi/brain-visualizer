2a8d062b4d4d7617804276f5699e2f2b88804e85
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-10-02 13:15:30 -0400

    Create DraggableWindow Middle Class

---
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/DraggableWindow.gd
@@ -0,0 +1,23 @@
+extends GrowingPanel
+class_name DraggableWindow
+
+signal closed_window(window_name: StringName)
+
+func _ready() -> void:
+	super._ready()
+	var title_bar: TitleBar = $TitleBar
+	title_bar.close_pressed.connect(_user_closed_window)
+
+func close_window(window_name: StringName) -> void:
+	print("WINDOWS UI: User closed %s window" % window_name)
+	closed_window.emit(window_name)
+
+func save_to_memory() -> Dictionary:
+	assert(false, "DraggableWindow 'save_to_memory' has not been overridden!")
+	return {}
+
+func load_from_memory(_previous_data: Dictionary) -> void:
+	assert(false, "DraggableWindow 'load_from_memory' has not been overridden!")
+
+func _user_closed_window() -> void:
+	assert(false, "DraggableWindow '_user_closed_window' has not been overridden!")
