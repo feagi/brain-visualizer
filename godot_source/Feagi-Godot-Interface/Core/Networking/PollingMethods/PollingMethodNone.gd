d3886d1b58bbc3e70a01020420ed452756b8a3a6
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-10-03 14:27:29 -0400

    modified none polling method to have definable return boolean

--- godot_source/Feagi-Godot-Interface/Core/Networking/PollingMethods/PollingMethodNone.gd
+++ godot_source/Feagi-Godot-Interface/Core/Networking/PollingMethods/PollingMethodNone.gd
@@ -1,6 +1,11 @@
 extends PollingMethodInterface
 class_name PollingMethodNone
-## Use for when there is no polling, immediately end the call
+## Use for when there is no polling, immediately end the call with the init bool
+
+var _output_bool: bool
+
+func _init(output_bool: bool) -> void:
+    _output_bool = output_bool
 
 ## Can't get easier than this
 func confirm_complete(_response_code: int, _response_body: PackedByteArray) -> bool:
