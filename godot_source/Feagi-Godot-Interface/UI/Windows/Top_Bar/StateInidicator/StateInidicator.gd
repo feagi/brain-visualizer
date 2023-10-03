6a75c7e72bc027eb62f9465ef0ba2aebd8bf2fbe
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-10-03 14:27:11 -0400

    added initial health indicators

---
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Top_Bar/StateInidicator/StateInidicator.gd
@@ -0,0 +1,19 @@
+extends Control
+class_name StateIndicator
+
+var _burst_engine: BooleanIndicator
+var _genome_availibility: BooleanIndicator
+var _genome_validity: BooleanIndicator
+var _brain_readiness: BooleanIndicator
+
+func _ready():
+	_burst_engine = $BurstEngine
+	_genome_availibility = $GenomeAvailability
+	_genome_validity = $GenomeValidity
+	_brain_readiness = $BrainReadiness
+
+func set_health_states(burst_engine: bool, genome_availibility: bool, genome_validity: bool, brain_readiness: bool) -> void:
+	_burst_engine.boolean_state = burst_engine
+	_genome_availibility.boolean_state = genome_availibility
+	_genome_validity.boolean_state = genome_validity
+	_brain_readiness.boolean_state = brain_readiness
