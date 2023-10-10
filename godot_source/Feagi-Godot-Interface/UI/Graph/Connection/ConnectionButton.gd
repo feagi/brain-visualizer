e301da22ad0027a5d3b6e493e52d542c1ea18d35
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-10-10 19:03:29 -0400

    Button Responds to Mapping Count Changes

--- godot_source/Feagi-Godot-Interface/UI/Graph/Connection/ConnectionButton.gd
+++ godot_source/Feagi-Godot-Interface/UI/Graph/Connection/ConnectionButton.gd
@@ -11,6 +11,7 @@ func setup(source_node: CorticalNode, destination_node: CorticalNode, number_map
 	_destination_node = destination_node
 	_source_node.position_offset_changed.connect(update_position)
 	_destination_node.position_offset_changed.connect(update_position)
+	_source_node.cortical_area_ref.efferent_area_count_updated.connect(_feagi_updated_a_mapping_count)
 	_label = get_child(0)
 	update_position()
 	update_mapping_counter(number_mappings)
@@ -30,3 +31,8 @@ func destroy_self() -> void:
 
 func _button_pressed() -> void:
 	VisConfig.UI_manager.window_manager.spawn_edit_mappings(_source_node.cortical_area_ref, _destination_node.cortical_area_ref)
+
+## Confirm efferent area is the one this conneciton represents, and update the mapping count
+func _feagi_updated_a_mapping_count(efferent_area: CorticalArea, mapping_count: int) -> void:
+	if efferent_area.cortical_ID == _destination_node.cortical_area_ID:
+		update_mapping_counter(mapping_count)
