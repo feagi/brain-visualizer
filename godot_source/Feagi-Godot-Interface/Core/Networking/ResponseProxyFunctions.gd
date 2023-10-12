b253b575ebbeba3b0c513e72262314bc91966148
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-10-12 18:55:51 -0400

    remove test cases

--- godot_source/Feagi-Godot-Interface/Core/Networking/ResponseProxyFunctions.gd
+++ godot_source/Feagi-Godot-Interface/Core/Networking/ResponseProxyFunctions.gd
@@ -313,7 +313,7 @@ func PUT_GE_morphology(_response_code: int, _response_body: PackedByteArray, cha
 ## returns nothing, so we passthrough the deleted cortical ID
 func DELETE_GE_corticalArea(_response_code: int, _response_body: PackedByteArray, deleted_cortical_ID: StringName) -> void:
 	print("FEAGI confirmed deletion of cortical area " + deleted_cortical_ID)
-	VisConfig.UI_manager.make_notification("Cortical Area Deleted!")
+	VisConfig.UI_manager.make_notification("Cortical Area %s Deleted!" %FeagiCache.cortical_areas_cache.cortical_areas[deleted_cortical_ID].name)
 	FeagiCache.cortical_areas_cache.remove_cortical_area(deleted_cortical_ID)
 
 func DELETE_GE_morphology(_response_code: int, _response_body: PackedByteArray, deleted_morphology_name: StringName) -> void:
