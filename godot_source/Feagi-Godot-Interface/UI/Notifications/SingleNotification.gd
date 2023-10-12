1d840eb94ef1ae23d294fca81d646325f770d84f
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-10-12 18:29:08 -0400

    Create Single Notification Object

---
+++ godot_source/Feagi-Godot-Interface/UI/Notifications/SingleNotification.gd
@@ -0,0 +1,29 @@
+extends PanelContainer
+class_name SingleNotification
+
+signal notification_closed(height: int)
+
+var _fancyText: RichTextLabel
+var _timer: Timer
+var _move_timer: Timer
+
+func _ready():
+	_fancyText = $Notification/RichTextLabel
+	_timer = $Notification/notification_timer
+	_move_timer = $Notification/move_timer
+	_timer.one_shot = true
+	_timer.timeout.connect(_closing)
+
+func set_notification(message: StringName, Y_offset: int, time_seconds: float = 5.0) -> void:
+	_fancyText.text = message
+	_timer.wait_time = time_seconds
+	_timer.start()
+	position = Vector2(position.x, Y_offset)
+
+func move_up_by(value: int):
+	position = Vector2(position.x, value)
+
+func _closing():
+	notification_closed.emit(size.y)
+	queue_free()
+	
