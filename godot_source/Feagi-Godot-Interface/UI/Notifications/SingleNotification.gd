c46fe1cd88a04bde7ecb4fb72752cb242af7de96
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-10-16 11:46:37 -0400

    Added color notification type support

--- godot_source/Feagi-Godot-Interface/UI/Notifications/SingleNotification.gd
+++ godot_source/Feagi-Godot-Interface/UI/Notifications/SingleNotification.gd
@@ -1,6 +1,18 @@
 extends PanelContainer
 class_name SingleNotification
 
+const type_colors: Dictionary = {
+	NOTIFICATION_TYPE.INFO : Color(0.32, 0.32, 0.32, 0.65),
+	NOTIFICATION_TYPE.WARNING: Color(0.87, 0.84, 0.28, 0.65),
+	NOTIFICATION_TYPE.ERROR: Color(0.84, 0.0, 0.0, 0.65)
+}
+
+enum NOTIFICATION_TYPE {
+	INFO,
+	WARNING,
+	ERROR
+}
+
 signal notification_closed(height: int)
 
 var _fancyText: RichTextLabel
@@ -14,10 +26,14 @@ func _ready():
 	_timer.one_shot = true
 	_timer.timeout.connect(_closing)
 
-func set_notification(message: StringName, time_seconds) -> void:
+func set_notification(message: StringName, time_seconds, notification_type: NOTIFICATION_TYPE = NOTIFICATION_TYPE.INFO) -> void:
 	_fancyText.text = message
 	_timer.wait_time = time_seconds
 	_timer.start()
+	var stylebox: StyleBoxFlat = StyleBoxFlat.new()
+	stylebox.bg_color = type_colors[notification_type]
+	self.add_theme_stylebox_override("panel", stylebox)
+	
 
 func move_up_by(value: int):
 	position = Vector2(position.x, value)
