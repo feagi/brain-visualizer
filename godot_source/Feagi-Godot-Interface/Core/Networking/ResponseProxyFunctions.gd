b5f8821862c3cd2faa6d010f9ff8e99839dbdf30
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-10-19 15:46:10 -0400

    Added Call for mass 2D update

--- godot_source/Feagi-Godot-Interface/Core/Networking/ResponseProxyFunctions.gd
+++ godot_source/Feagi-Godot-Interface/Core/Networking/ResponseProxyFunctions.gd
@@ -303,8 +303,8 @@ func PUT_GE_mappingProperties(_response_code: int, _response_body: PackedByteArr
 	cortical_src.set_efferent_connection(cortical_dst, mapping_count)
 
 
-func PUT_GE_corticalArea(_response_code: int, _response_body: PackedByteArray, changed_cortical_ID: StringName) -> void:
-	if _response_code == 422:
+func PUT_GE_corticalArea(response_code: int, _response_body: PackedByteArray, changed_cortical_ID: StringName) -> void:
+	if response_code == 422:
 		push_error("Unable to process new properties for cortical area %s, skipping!" % [changed_cortical_ID])
 		return
 	
@@ -312,11 +312,15 @@ func PUT_GE_corticalArea(_response_code: int, _response_body: PackedByteArray, c
 	FeagiRequests.refresh_cortical_area(FeagiCache.cortical_areas_cache.cortical_areas[changed_cortical_ID], true)
 	pass
 
-func PUT_GE_morphology(_response_code: int, _response_body: PackedByteArray, changed_morphology_name: StringName) -> void:
-	if _response_code == 422:
+func PUT_GE_morphology(response_code: int, _response_body: PackedByteArray, changed_morphology_name: StringName) -> void:
+	if response_code == 422:
 		push_error("Unable to process new properties for morphology %s, skipping!" % [changed_morphology_name])
 		return
 	FeagiRequests.refresh_morphology_properties(changed_morphology_name)
+
+func PUT_GE_coord2D(_response_code: int, _response_body: PackedByteArray, changed_IDs_and_locations: Dictionary) -> void:
+	# TODO catch errors - at this time feagi only returns 200
+	FeagiCache.cortical_areas_cache.FEAGI_mass_update_2D_positions(changed_IDs_and_locations)
 
 ## returns nothing, so we passthrough the deleted cortical ID
 func DELETE_GE_corticalArea(_response_code: int, _response_body: PackedByteArray, deleted_cortical_ID: StringName) -> void:
