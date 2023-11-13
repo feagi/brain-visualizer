f13679958dc57b6684126f7b4a9379da61679ee2
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-11-13 11:11:05 -0500

    Delete cortical box on reload

--- godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/CorticalBox/CorticalBoxPreview.gd
+++ godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/CorticalBox/CorticalBoxPreview.gd
@@ -2,10 +2,13 @@ extends MeshInstance3D
 class_name CorticalBoxPreview
 ## Acts as a hologram to show where and how big a generated cortical area will be
 
+func _ready() -> void:
+	FeagiEvents.genome_is_about_to_reset.connect(window_closed) # On genome reset, reuse the window close function to delete the preview
+
 func toggle_rendering(is_rendering: bool) -> void:
 	visible = is_rendering
 
-func window_closed(irrelevant) -> void:
+func window_closed(irrelevant = null) -> void:
 	queue_free()
 
 func update_size(new_size: Vector3) -> void:
