9031db2e2702fd07be948a161b54fae71235b620
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-11-21 12:54:27 -0500

    Add methods to check if mappings are excitable or plastic

--- godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Cortical-Areas/Mappings/MappingProperties.gd
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Cortical-Areas/Mappings/MappingProperties.gd
@@ -20,6 +20,26 @@ func _init(source_area: CorticalArea, destination_area: CorticalArea, mappings_b
 	_dst_cortical = destination_area
 	_mappings = mappings_between_them
 
+## Given the dictionary from the FEAGI mapping properties call directly creates a MappingProperties object. Yes the spelling is correct
+static func from_MappingPropertys(mapping_properties_from_FEAGI: Array, source_area: CorticalArea, destination_area: CorticalArea) -> MappingProperties:
+	var new_mappings: Array[MappingProperty] = []
+	for raw_mappings in mapping_properties_from_FEAGI:
+		if raw_mappings["morphology_id"] not in FeagiCache.morphology_cache.available_morphologies.keys():
+			push_error("Unable to add specific mapping due to missing morphology %s in the internal cache! Skipping!" % [raw_mappings["morphology_id"]])
+			continue
+		new_mappings.append(MappingProperty.from_dict(raw_mappings))
+	return MappingProperties.new(source_area, destination_area, new_mappings)
+
+## Creates an empty mapping between a source and destination cortical area
+static func create_empty_mapping(source_area: CorticalArea, destination_area: CorticalArea) -> MappingProperties:
+	var empty_typed_array: Array[MappingProperty] = [] # Because the array type casting in godot is still stupid. Too Bad!
+	return MappingProperties.new(source_area, destination_area, empty_typed_array)
+
+## Creates a default mapping object given a source, destination, and morphology to use. Default settings will be used
+static func create_default_mapping(source_area: CorticalArea, destination_area: CorticalArea, morphology_to_use: Morphology) -> MappingProperties:
+	var default_mapping: Array[MappingProperty] = [MappingProperty.create_default_mapping(morphology_to_use)]
+	return MappingProperties.new(source_area, destination_area, default_mapping)
+
 ## Returns an array of the internal [MappingProperty] objects as FEAGI formatted dictionaries
 func to_array() -> Array[Dictionary]:
 	var output: Array[Dictionary] = []
@@ -48,23 +68,18 @@ func merge_in_mapping_properties(to_merge_in: MappingProperties) -> void:
 		push_error("Unable to merge MappingProperties with different destination areas! Skipping merge!")
 		return
 	_mappings.append_array(to_merge_in.mappings)
-	
-## Given the dictionary from the FEAGI mapping properties call directly creates a MappingProperties object. Yes the spelling is correct
-static func from_MappingPropertys(mapping_properties_from_FEAGI: Array, source_area: CorticalArea, destination_area: CorticalArea) -> MappingProperties:
-	var new_mappings: Array[MappingProperty] = []
-	for raw_mappings in mapping_properties_from_FEAGI:
-		if raw_mappings["morphology_id"] not in FeagiCache.morphology_cache.available_morphologies.keys():
-			push_error("Unable to add specific mapping due to missing morphology %s in the internal cache! Skipping!" % [raw_mappings["morphology_id"]])
-			continue
-		new_mappings.append(MappingProperty.from_dict(raw_mappings))
-	return MappingProperties.new(source_area, destination_area, new_mappings)
 
-## Creates an empty mapping between a source and destination cortical area
-static func create_empty_mapping(source_area: CorticalArea, destination_area: CorticalArea) -> MappingProperties:
-	var empty_typed_array: Array[MappingProperty] = [] # Because the array type casting in godot is still stupid. Too Bad!
-	return MappingProperties.new(source_area, destination_area, empty_typed_array)
+## Returns true if any other internal mappings are plastic
+func is_any_mapping_plastic() -> bool:
+	for mapping: MappingProperty in mappings:
+		if mapping.is_plastic:
+			return true
+	return false
 
-## Creates a default mapping object given a source, destination, and morphology to use. Default settings will be used
-static func create_default_mapping(source_area: CorticalArea, destination_area: CorticalArea, morphology_to_use: Morphology) -> MappingProperties:
-	var default_mapping: Array[MappingProperty] = [MappingProperty.create_default_mapping(morphology_to_use)]
-	return MappingProperties.new(source_area, destination_area, default_mapping)
+## Returns true if any mapping's PSP multiplier is positive
+func is_any_PSP_multiplier_positive() -> bool:
+	for mapping: MappingProperty in mappings:
+		if mapping.post_synaptic_current_multiplier > 0.0:
+			return true
+	return false
+
