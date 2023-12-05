5a30a5192f1a9da70ec2cf1a2f8e1132a8a11588
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-12-04 19:06:51 -0500

    remove dead code from COrticalNodeGraph

--- godot_source/Feagi-Godot-Interface/UI/Graph/CorticalNodeGraph.gd
+++ godot_source/Feagi-Godot-Interface/UI/Graph/CorticalNodeGraph.gd
@@ -27,7 +27,6 @@ func _ready():
 	_move_timer.timeout.connect(_move_timer_finished)
 
 	connection_request.connect(_user_request_connection)
-	disconnection_request.connect(_user_request_connection_deletion)
 
 
 ## Spawns a cortical Node, should only be called via FEAGI
@@ -57,33 +56,7 @@ func feagi_deleted_single_cortical_node(cortical_area: CorticalArea) -> void:
 	var node: CorticalNode = cortical_nodes[cortical_area.cortical_ID]
 	node.FEAGI_delete_cortical_area()
 	cortical_nodes.erase(cortical_area.cortical_ID)
-
-## Spawns a conneciton button, which itself coordinates maintaining connection lines 
-func feagi_create_connection_button_from_efferent(mapping_properties: MappingProperties) -> void:
 	
-	
-	pass
-	if mapping_properties.source_cortical_area.cortical_ID not in _connection_buttons.keys():
-		_connection_buttons[mapping_properties.source_cortical_area.cortical_ID] = {}
-	if mapping_properties.destination_cortical_area.cortical_ID in _connection_buttons[mapping_properties.source_cortical_area.cortical_ID].keys():
-		# Button Exists. Do nothing
-		return
-	
-	if mapping_properties.source_cortical_area.cortical_ID not in cortical_nodes.keys():
-		push_error("UI: GRAPH: Unable to create a connection due to missing cortical area %s! Skipping!" % mapping_properties.source_cortical_area.cortical_ID)
-		return
-	if mapping_properties.destination_cortical_area.cortical_ID not in cortical_nodes.keys():
-		push_error("GRAPH: Unable to create a connection due to missing cortical area %s! Skipping!" % mapping_properties.destination_cortical_area.cortical_ID)
-		return
-
-	var source_node: CorticalNode = cortical_nodes[mapping_properties.source_cortical_area.cortical_ID]
-	var destination_node: CorticalNode = cortical_nodes[mapping_properties.destination_cortical_area.cortical_ID]
-	
-	# Spawn Button and setup
-	_connection_buttons[mapping_properties.source_cortical_area.cortical_ID][mapping_properties.destination_cortical_area.cortical_ID] = intercortical_connection_prefab.instantiate()
-	_connection_buttons[mapping_properties.source_cortical_area.cortical_ID][mapping_properties.destination_cortical_area.cortical_ID].setup(source_node, destination_node, mapping_properties, self)
-
-
 ## Every time a cortical node moves, store and send it when time is ready
 func _cortical_node_moved(node: CorticalNode, new_position: Vector2i) -> void:
 	print("Buffering change in position of cortical area " + node.cortical_area_ID)
@@ -98,13 +71,9 @@ func _move_timer_finished():
 	FeagiRequests.request_mass_change_2D_positions(_moved_cortical_areas_buffer)
 	_moved_cortical_areas_buffer = {}
 
-
 ## User requested a connection. Note that this function is going to be redone in the graph edit refactor
 func _user_request_connection(from_cortical_ID: StringName, _from_port: int, to_cortical_ID: StringName, _to_port: int) -> void:
 	VisConfig.UI_manager.window_manager.spawn_edit_mappings(FeagiCache.cortical_areas_cache.cortical_areas[from_cortical_ID], FeagiCache.cortical_areas_cache.cortical_areas[to_cortical_ID])
-
-func _user_request_connection_deletion(from_cortical_ID: StringName, _from_port: int, to_cortical_ID: StringName, _to_port: int) -> void:
-	FeagiRequests.request_delete_mapping_between_corticals(FeagiCache.cortical_areas_cache.cortical_areas[from_cortical_ID], FeagiCache.cortical_areas_cache.cortical_areas[to_cortical_ID])
 
 func _on_genome_reset():
 	_spawn_sorter = CorticalNodeSpawnSorter.new(algorithm_cortical_area_spacing, NODE_SIZE)
