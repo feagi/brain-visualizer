ab1af0b615f9443d9706b428684811f1b7dacb1d
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-12-11 14:46:55 -0500

    Rebuilt top of left bar

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Left_Bar/Parts/LeftBarCorticalParameters.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Left_Bar/Parts/LeftBarCorticalParameters.gd
@@ -1,5 +1,5 @@
 extends VBoxContainer
-class_name LeftBarTop
+class_name LeftBarCorticalParameters
 ## Top Section of the Left Bar Window
 ## TODO add field color changing
 
@@ -10,6 +10,8 @@ var top_panel: WindowLeftPanel
 var _line_cortical_name: TextInput
 var _line_cortical_ID: TextInput
 var _line_cortical_type: TextInput
+var _line_voxel_neuron_density: IntInput
+var _line_synaptic_attractivity: IntInput
 var _vector_position: Vector3iSpinboxField
 var _vector_dimensions: Vector3iSpinboxField
 var _update_button: TextButton_Element
@@ -20,6 +22,8 @@ func _ready():
 	_line_cortical_name = $Row_Cortical_Name/Cortical_Name
 	_line_cortical_ID = $Row_Cortical_ID/Cortical_ID
 	_line_cortical_type = $Row_Cortical_Type/Cortical_Type
+	_line_voxel_neuron_density = $Row_Voxel_Neuron_Density/Voxel_Neuron_Density
+	_line_synaptic_attractivity = $Synaptic_Attractivity/Synaptic_Attractivity
 	_vector_position = $Cortical_Position
 	_vector_dimensions = $Cortical_Size
 	_update_button = $Update_Button
@@ -29,30 +33,42 @@ func _ready():
 	_vector_dimensions.user_updated_vector.connect(_user_edit_dimension)
 	_update_button.pressed.connect(_user_requests_update)
 
-## set initial values from FEAGI Cache
-func initial_values_from_FEAGI(cortical_reference: BaseCorticalArea) -> void:
+## Displays properties of a cortical area, toggles editability depending on corticala rea configuraiton
+func display_cortical_properties(cortical_reference: BaseCorticalArea) -> void:
 	_line_cortical_name.text = cortical_reference.name
 	_line_cortical_ID.text = cortical_reference.cortical_ID
-	_line_cortical_type.text = BaseCorticalArea.CORTICAL_AREA_TYPE.keys()[cortical_reference.group]
-	_vector_position.current_vector = cortical_reference.coordinates_3D
+	_line_cortical_type.text = BaseCorticalArea.cortical_type_to_str(cortical_reference.group)
+	_line_voxel_neuron_density.current_int = cortical_reference.cortical_neuron_per_vox_count
+	_line_synaptic_attractivity.current_int = cortical_reference.cortical_synaptic_attractivity
 	_vector_dimensions.current_vector = cortical_reference.dimensions
-	if cortical_reference.is_dimension_not_editable:
-		_vector_dimensions.editable = false
+	_vector_position.current_vector = cortical_reference.coordinates_3D
+	
+	_line_cortical_name.editable = cortical_reference.user_can_edit_name
+	_line_voxel_neuron_density.editable = cortical_reference.user_can_edit_cortical_neuron_per_vox_count
+	_line_synaptic_attractivity.editable = cortical_reference.user_can_edit_cortical_synaptic_attractivity
+	_vector_dimensions.editable = cortical_reference.user_can_edit_dimensions
+
 
 func FEAGI_set_cortical_name(new_name: StringName, _duplicate_ref: BaseCorticalArea):
-	print("Left pane recieved new cortical name")
 	_line_cortical_name.text = new_name
 	_FEAGI_confirmed_update()
 
-func FEAGI_set_cortical_position(new_position: Vector3i, _duplicate_ref: BaseCorticalArea):
-	print("Left pane recieved new cortical position")
-	_vector_position.current_vector = new_position
+func FEAGI_set_voxel_neuron_density(new_val: int, _duplicate_ref: BaseCorticalArea):
+	_line_voxel_neuron_density.current_int = new_val
+	_FEAGI_confirmed_update()
+
+func FEAGI_set_synaptic_attractivity(new_val: int, _duplicate_ref: BaseCorticalArea):
+	_line_synaptic_attractivity.current_int = new_val
 	_FEAGI_confirmed_update()
 
 func FEAGI_set_cortical_dimension(new_dimension: Vector3i, _duplicate_ref: BaseCorticalArea):
-	print("Left pane recieved new cortical dimensions")
 	_vector_dimensions.current_vector = new_dimension
 	_FEAGI_confirmed_update()
+
+func FEAGI_set_cortical_position(new_position: Vector3i, _duplicate_ref: BaseCorticalArea):
+	_vector_position.current_vector = new_position
+	_FEAGI_confirmed_update()
+
 
 ## FEAGI confirmed changes, show this in the UI and clear the backend dict
 func _FEAGI_confirmed_update() -> void:
@@ -64,23 +80,25 @@ func _FEAGI_confirmed_update() -> void:
 func _user_requests_update() -> void:
 	if _growing_cortical_update == {}:
 		# If user presses update button but no properties are set to change, do nothing
-		_update_button.disabled = true
 		return
 	print("User requesing Summary changes to cortical area")
 	user_requested_update.emit(_growing_cortical_update)
 
 func _user_edit_name(new_name: String) -> void:
-	print("User queued name change")
 	_growing_cortical_update["cortical_name"] = new_name
 
+func _user_edit_voxel_density(new_val: int) -> void:
+	_growing_cortical_update["cortical_neuron_per_vox_count"] = new_val
+
+func _user_edit_synaptic_attractivity(new_val: int) -> void:
+	_growing_cortical_update["cortical_synaptic_attractivity"] = new_val
+
 func _user_edit_3D_position(new_position: Vector3i) -> void:
-	print("User queued position change")
 	_growing_cortical_update["cortical_coordinates"] = FEAGIUtils.vector3i_to_array(new_position)
 	if !_is_preview_active:
 		_enable_3D_preview()
 
 func _user_edit_dimension(new_dimension: Vector3i) -> void:
-	print("User queued dimension change")
 	_growing_cortical_update["cortical_dimensions"] = FEAGIUtils.vector3i_to_array(new_dimension)
 	if !_is_preview_active:
 		_enable_3D_preview()
