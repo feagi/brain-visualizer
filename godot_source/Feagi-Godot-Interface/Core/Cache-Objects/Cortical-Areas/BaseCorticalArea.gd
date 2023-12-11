5b1df3b9b7bb68c46043e8dad97651e78f8af0e7
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-12-11 12:52:06 -0500

    Made post synaptic section have its own signals

--- godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Cortical-Areas/BaseCorticalArea.gd
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Cortical-Areas/BaseCorticalArea.gd
@@ -400,11 +400,79 @@ func get_allowed_efferent_morphology_names() -> PackedStringArray:
 #endregion
 
 #region Post Synaptic Potential Parameters
-var neuron_psp_uniform_distribution: bool = false
-var neuron_mp_driven_psp: bool = false
-var neuron_post_synaptic_potential: float = 0.0
-var neuron_post_synaptic_potential_max: float = 0.0
-var neuron_degeneracy_coefficient: int = 0
+
+signal neuron_psp_uniform_distribution_updated(new_val: bool, this_cortical_area: BaseCorticalArea)
+signal neuron_neuron_mp_driven_psp_updated(new_val: bool, this_cortical_area: BaseCorticalArea)
+signal neuron_post_synaptic_potential_updated(new_val: float, this_cortical_area: BaseCorticalArea)
+signal neuron_post_synaptic_potential_max_updated(new_val: float, this_cortical_area: BaseCorticalArea)
+signal neuron_degeneracy_coefficient_updated(new_val: int, this_cortical_area: BaseCorticalArea)
+
+var neuron_psp_uniform_distribution: bool:
+	get:
+		return _neuron_psp_uniform_distribution
+	set(v):
+		_set_neuron_psp_uniform_distribution(v)
+
+var neuron_mp_driven_psp: bool:
+	get:
+		return _neuron_mp_driven_psp
+	set(v):
+		_set_neuron_mp_driven_psp(v)
+
+var neuron_post_synaptic_potential: float:
+	get:
+		return _neuron_post_synaptic_potential
+	set(v):
+		_set_neuron_post_synaptic_potential(v)
+
+var neuron_post_synaptic_potential_max: float:
+	get:
+		return _neuron_post_synaptic_potential_max
+	set(v):
+		_set_neuron_post_synaptic_potential_max(v)
+
+var neuron_degeneracy_coefficient: int:
+	get:
+		return _neuron_degeneracy_coefficient
+	set(v):
+		_set_neuron_degeneracy_coefficient(v)
+
+var _neuron_psp_uniform_distribution: bool = false
+var _neuron_mp_driven_psp: bool = false
+var _neuron_post_synaptic_potential: float = 0.0
+var _neuron_post_synaptic_potential_max: float = 0.0
+var _neuron_degeneracy_coefficient: int = 0
+
+func _set_neuron_psp_uniform_distribution(new_val: bool) -> void:
+	if new_val == _neuron_psp_uniform_distribution: 
+		return
+	_neuron_psp_uniform_distribution = new_val
+	neuron_psp_uniform_distribution_updated.emit(new_val, self)
+
+func _set_neuron_mp_driven_psp(new_val: bool) -> void:
+	if new_val == _neuron_mp_driven_psp: 
+		return
+	neuron_mp_driven_psp = new_val
+	neuron_neuron_mp_driven_psp_updated.emit(new_val, self)
+
+func _set_neuron_post_synaptic_potential(new_val: float) -> void:
+	if new_val == _neuron_post_synaptic_potential: 
+		return
+	_neuron_post_synaptic_potential = new_val
+	neuron_post_synaptic_potential_updated.emit(new_val, self)
+
+func _set_neuron_post_synaptic_potential_max(new_val: float) -> void:
+	if new_val == _neuron_post_synaptic_potential_max: 
+		return
+	_neuron_post_synaptic_potential_max = new_val
+	neuron_post_synaptic_potential_max_updated.emit(new_val, self)
+
+func _set_neuron_degeneracy_coefficient(new_val: int) -> void:
+	if new_val == _neuron_degeneracy_coefficient: 
+		return
+	_neuron_degeneracy_coefficient = new_val
+	neuron_degeneracy_coefficient_updated.emit(new_val, self)
+
 #endregion
 
 # Monitoring settings for this specific cortical area
