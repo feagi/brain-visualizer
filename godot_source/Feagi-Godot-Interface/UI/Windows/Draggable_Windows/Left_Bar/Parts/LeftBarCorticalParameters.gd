bac5db7ce703197b9b0c7a78e23285397c389085
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-12-14 16:42:26 -0500

    fix incorrect synaptic accritivty signal

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Left_Bar/Parts/LeftBarCorticalParameters.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Left_Bar/Parts/LeftBarCorticalParameters.gd
@@ -30,7 +30,7 @@ func _ready():
 
 	_line_cortical_name.text_confirmed.connect(_user_edit_name)
 	_line_voxel_neuron_density.int_confirmed.connect(_user_edit_voxel_density)
-	_line_synaptic_attractivity.int_confirmed.connect(FEAGI_set_synaptic_attractivity)
+	_line_synaptic_attractivity.int_confirmed.connect(_user_edit_synaptic_attractivity)
 	_vector_position.user_updated_vector.connect(_user_edit_3D_position)
 	_vector_dimensions.user_updated_vector.connect(_user_edit_dimension)
 	_update_button.pressed.connect(_user_requests_update)
