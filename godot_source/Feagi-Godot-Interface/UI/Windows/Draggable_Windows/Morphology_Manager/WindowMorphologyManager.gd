3fd900c965438686839ca0ee6dcf599ecfe62527
Amir-Rasteg <amir.rasteg@protonmail.com>
2023-12-21 20:32:19 -0500

    refactor deletion

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Morphology_Manager/WindowMorphologyManager.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Morphology_Manager/WindowMorphologyManager.gd
@@ -25,14 +25,14 @@ func load_morphology(morphology: Morphology, update_FEAGI_cache: bool = true) ->
 	_selected_morphology = morphology
 	_smart_morphology_view.load_in_morphology(morphology)
 	_morphology_generic_details.load_in_morphology(morphology)
-	_delete_morphology_button.disabled = !_selected_morphology.is_user_editable
+	_automatically_set_delete_button_availability()
 	if update_FEAGI_cache:
 		FeagiRequests.refresh_morphology_properties(morphology.name)
 
 func set_selected_morphology(morphology: Morphology) -> void:
 	_morphology_scroll.select_morphology(morphology) # fires back here
 
-## FEAGI sent back bew morphlogy values. Update
+## FEAGI sent back new morphlogy values. Update
 func morphology_updated_from_FEAGI(updated_morphology: Morphology) -> void:
 	if _selected_morphology.name != updated_morphology.name:
 		return
@@ -49,6 +49,10 @@ func send_updated_values_to_feagi() -> void:
 	var morphology_to_send: Morphology = _smart_morphology_view.retrieve_morphology(_selected_morphology.name, _morphology_generic_details.details_text)
 	FeagiRequests.request_updating_morphology(morphology_to_send)
 
+func _automatically_set_delete_button_availability() -> void:
+	_delete_morphology_button.disabled = (!_selected_morphology.is_user_editable) or (!_selected_morphology.is_being_used)
 
+func _request_delete_selected_morphology() -> void:
+	FeagiRequests.request_delete_morphology(_selected_morphology)
 
 
