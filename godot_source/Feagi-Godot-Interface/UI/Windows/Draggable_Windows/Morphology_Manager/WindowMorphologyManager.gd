e0714434b68a3b2c4e9c05dcdf1787e42f80dad0
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-01-05 20:09:45 -0500

    fixed window stuff

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Morphology_Manager/WindowMorphologyManager.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Morphology_Manager/WindowMorphologyManager.gd
@@ -1,10 +1,13 @@
 extends DraggableWindow
 class_name WindowMorphologyManager
 
+var _UI_morphology_overviews: UIMorphologyOverviews
 
 func _ready() -> void:
 	super()
+	_UI_morphology_overviews = $UIMorphologyOverviews
+	_UI_morphology_overviews.minimum_size_changed.connect(_update_size)
+	_UI_morphology_overviews.request_close.connect(VisConfig.UI_manager.window_manager.force_close_window.bind("morphology_manager"))
 
-
-	
-
+func _update_size() -> void:
+	size = _UI_morphology_overviews.size
