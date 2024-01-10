fb0c1f54708417fbf3ce02d31a43a7c8ca72efc5
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-01-10 13:50:48 -0500

    Optimize freeing of preview handler

--- godot_source/Feagi-Godot-Interface/UI/Universal/GenericSinglePreviewHandler.gd
+++ godot_source/Feagi-Godot-Interface/UI/Universal/GenericSinglePreviewHandler.gd
@@ -15,9 +15,9 @@ func connect_BM_preview(coordinates_3D_changed: Signal, dimensions_changed: Sign
 	coordinates_3D_changed.connect(_BM_preview.update_position)
 	dimensions_changed.connect(_BM_preview.update_size)
 	for close_signal in close_signals: #TODO this isnt bm specific!
-		close_signal.connect(_BM_preview.delete_preview)
+		close_signal.connect(delete_preview)
 
 func delete_preview() -> void:
 	if _BM_preview != null:
 		_BM_preview.delete_preview()
-	free()
+	#free() #TODO
