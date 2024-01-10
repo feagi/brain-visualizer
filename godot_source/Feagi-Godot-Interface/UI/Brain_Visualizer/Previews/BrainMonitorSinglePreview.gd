9af12157bc70c100eb03a54a0760729ba041882e
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-01-10 13:30:06 -0500

    Fix material type reference

--- godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/Previews/BrainMonitorSinglePreview.gd
+++ godot_source/Feagi-Godot-Interface/UI/Brain_Visualizer/Previews/BrainMonitorSinglePreview.gd
@@ -8,7 +8,7 @@ const DEFAULT_COLOR: Color = Color(0.133, 0.733, 0.114, 0.8)
 
 const COLOR_PARAMETER_NAME: StringName = "albedo_color"
 
-var _material: Material
+var _material: StandardMaterial3D
 
 func _ready() -> void:
 	FeagiEvents.genome_is_about_to_reset.connect(delete_preview) # On genome reset, reuse the window close function to delete the preview
@@ -33,4 +33,4 @@ func update_position(new_position: Vector3) -> void:
 	position = BaseCorticalArea.true_position_to_BV_position(new_position, scale) #TODO this func needs to be moved to BMM
 
 func set_color(new_color: Color) -> void:
-	_material.set_shader_parameter("COLOR_PARAMETER_NAME", new_color)
+	_material.albedo_color = new_color
