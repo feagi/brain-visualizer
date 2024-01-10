e138ed834f66912d0dcd13e9b0e38a9d24522bc8
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-01-10 13:39:07 -0500

    ported left bar to new system

--- godot_source/Feagi-Godot-Interface/UI/Universal/GenericSinglePreviewHandler.gd
+++ godot_source/Feagi-Godot-Interface/UI/Universal/GenericSinglePreviewHandler.gd
@@ -14,9 +14,10 @@ func connect_BM_preview(coordinates_3D_changed: Signal, dimensions_changed: Sign
 		return
 	coordinates_3D_changed.connect(_BM_preview.update_position)
 	dimensions_changed.connect(_BM_preview.update_size)
-	for close_signal in close_signals:
+	for close_signal in close_signals: #TODO this isnt bm specific!
 		close_signal.connect(_BM_preview.delete_preview)
 
 func delete_preview() -> void:
 	if _BM_preview != null:
 		_BM_preview.delete_preview()
+	free()
