6d4e532a69a5fc162f1c92086c5ab6ba9c6672ef
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-01-22 18:24:13 -0500

    Added Neuron excitability to UI

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Left_Bar/Parts/LeftBarNeuronFiringParameters.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Left_Bar/Parts/LeftBarNeuronFiringParameters.gd
@@ -6,6 +6,7 @@ signal user_requested_update(changed_values: Dictionary)
 
 var _Fire_Threshold: IntInput
 var _Threshold_Limit: IntInput
+var _neuron_excitability: IntInput
 var _Refactory_Period: IntInput
 var _Leak_Constant: IntInput
 var _Leak_Variability: IntInput
@@ -28,6 +29,7 @@ func _ready():
 	_Consecutive_Fire_Count = $Consecutive_Fire_Count/Consecutive_Fire_Count
 	_Snooze_Period = $Snooze_Period/Snooze_Period
 	_MP_Accumulation = $MP_Accumulation/MP_Accumulation
+	_neuron_excitability = $Neuron_Excitability/Neuron_Excitability
 	_update_button = $Update_Button
 	
 	_Fire_Threshold.int_confirmed.connect(user_request_Fire_Threshold)
@@ -36,6 +38,7 @@ func _ready():
 	_Leak_Constant.int_confirmed.connect(user_request_Leak_Constant)
 	_Leak_Variability.int_confirmed.connect(user_request_Leak_Variability)
 	_Consecutive_Fire_Count.int_confirmed.connect(user_request_Consecutive_Fire_Count)
+	_neuron_excitability.int_confirmned.connect(_user_request_neuron_excitability)
 	_Snooze_Period.int_confirmed.connect(user_request_Snooze_Period)
 	_Threshold_Inc.user_updated_vector.connect(user_request_Threshold_Inc)
 	_MP_Accumulation.toggled.connect(user_request_MP_Accumumulation)
@@ -51,6 +54,7 @@ func display_cortical_properties(cortical_reference) -> void: #NOTE: Can't type 
 	_Consecutive_Fire_Count.current_int = cortical_reference.neuron_consecutive_fire_count
 	_Snooze_Period.current_int = cortical_reference.neuron_snooze_period
 	_Threshold_Inc.current_vector = cortical_reference.neuron_fire_threshold_increment
+	_neuron_excitability.current_int = cortical_reference.neuron_excitability
 	_MP_Accumulation.set_pressed_no_signal(cortical_reference.neuron_mp_charge_accumulation)
 	
 	cortical_reference.neuron_mp_charge_accumulation_updated.connect(_feagi_update_MP_Accumumulation)
@@ -61,6 +65,7 @@ func display_cortical_properties(cortical_reference) -> void: #NOTE: Can't type 
 	cortical_reference.neuron_snooze_period_updated.connect(_feagi_update_Snooze_Period)
 	cortical_reference.neuron_fire_threshold_updated.connect(_feagi_update_Fire_Threshold)
 	cortical_reference.neuron_firing_threshold_limit_updated.connect(_feagi_update_Threshold_Limit)
+	cortical_reference.neuron_excitability_updated.connect(_feagi_update_Neuron_Excitability)
 	cortical_reference.neuron_fire_threshold_increment_updated.connect(_feagi_update_Threshold_Inc)
 	
 	
@@ -100,7 +105,8 @@ func user_request_Snooze_Period(value: int) -> void:
 func user_request_MP_Accumumulation(value: bool) -> void:
 	_growing_cortical_update["neuron_mp_charge_accumulation"] = value
 
-
+func _user_request_neuron_excitability(value: int) -> void:
+	_growing_cortical_update["neuron_excitability"] = value
 
 
 func _feagi_update_Fire_Threshold(value: int, _cortical_ref) -> void:
@@ -126,6 +132,9 @@ func _feagi_update_Consecutive_Fire_Count(value: int, _cortical_ref) -> void:
 
 func _feagi_update_Snooze_Period(value: int, _cortical_ref) -> void:
 	_Snooze_Period.current_int = value
+
+func _feagi_update_Neuron_Excitability(value: int, _cortical_ref) -> void:
+	_neuron_excitability.current_int = value
 
 func _feagi_update_MP_Accumumulation(value: bool, _cortical_ref) -> void:
 	_MP_Accumulation.set_pressed_no_signal(value)
