57aa7fee14159c049ba29002251c3598c132763d
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-01-25 17:18:32 -0500

    Update Amalgamation Request

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Amalgamation_Request/WindowAmalgamationRequest.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Amalgamation_Request/WindowAmalgamationRequest.gd
@@ -1,4 +1,4 @@
-extends DraggableWindow
+extends BaseWindowPanel
 class_name WindowAmalgamationRequest
 
 signal null_dimchange_signal(val: Vector3i) # Not technically utilized, but needed as a placeholder as a required arg
@@ -12,7 +12,6 @@ var _preview_holder: GenericSinglePreviewHandler
 
 
 func _ready() -> void:
-	super._ready()
 	_field_title = $Container/HBoxContainer/AmalgamationTitle
 	_field_3d_location = $Container/HBoxContainer2/Coordinates_3D
 	_preview_holder = GenericSinglePreviewHandler.new()
@@ -23,18 +22,18 @@ func setup(amalgamation_ID: StringName, genome_title: StringName, circuit_size: 
 	_circuit_size = circuit_size
 	_field_title.text = genome_title
 	_preview_holder.start_BM_preview(_circuit_size, _field_3d_location.current_vector)
-	var closed_signals: Array[Signal] = [closed_window_no_name]
+	var closed_signals: Array[Signal] = [close_window_requested]
 	_preview_holder.connect_BM_preview(_field_3d_location.user_updated_vector, null_dimchange_signal, closed_signals)
 	
 func _cancel_pressed():
-	close_window("import_amalgamation")
+	close_window()
 
 func _import_pressed():
 	FeagiRequests.request_import_amalgamation(_field_3d_location.current_vector, _amalgamation_ID)
-	close_window("import_amalgamation", false)
+	close_window(false)
 
 #OVERRIDE
-func close_window(window_name: StringName, request_cancel: bool = true) -> void:
+func close_window(request_cancel: bool = true) -> void:
 	if request_cancel:
 		FeagiRequests.request_cancel_amalgamation(_amalgamation_ID)
-	super(window_name)
+	super()
