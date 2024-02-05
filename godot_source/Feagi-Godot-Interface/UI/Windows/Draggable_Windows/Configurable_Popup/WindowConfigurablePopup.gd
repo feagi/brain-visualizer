dc3c2073053264d9a718f799e0b6d1503c71fdfe
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-02-05 17:30:36 -0500

    move title generation to definition itself

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Configurable_Popup/WindowConfigurablePopup.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Configurable_Popup/WindowConfigurablePopup.gd
@@ -11,21 +11,12 @@ func _ready() -> void:
 	_button_container = $VBoxContainer/HBoxContainer
 
 func setup(popup_definition: ConfigurablePopupDefinition) -> void:
-	_setup_base_window("popup_" + _generate_random_letters(4)) # The popup should a unique ID TODO: come up witha  better method later
+	_setup_base_window(popup_definition.window_name) #TODO: come up witha  better method later
 	_title_bar.title = popup_definition.title
 	_message_box.text = popup_definition.message
 	_generate_buttons(popup_definition.buttons)
 	custom_minimum_size = popup_definition.minimum_size
-	
-#TODO this should probably be somewhere else, like in utils?
-func _generate_random_letters(num_letters: int) -> StringName:
-	var result: String = ""
-	for i in range(num_letters):
-		# Generate a random integer between 65 (A) and 90 (Z)
-		var random_ascii: int = randi() % (90 - 65 + 1) + 65
-		var random_letter: String = char(random_ascii)
-		result += random_letter
-	return result
+
 
 func _generate_buttons(button_definitions: Array[ConfigurableButtonDefinition]) -> void:
 	for definition in button_definitions:
--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Configurable_Popup/definitions/ConfigurablePopupDefinition.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Configurable_Popup/definitions/ConfigurablePopupDefinition.gd
@@ -6,15 +6,27 @@ var title: StringName
 var message: StringName
 var buttons: Array[ConfigurableButtonDefinition]
 var minimum_size: Vector2i
+var window_name: StringName
 
 func _init(window_title: StringName, window_message: StringName, window_buttons: Array[ConfigurableButtonDefinition], window_minumum_size: Vector2i = Vector2i(0,0) ) -> void:
 	title = window_title
 	message = window_message
 	buttons = window_buttons
 	minimum_size = window_minumum_size
+	window_name = "popup_" + _generate_random_letters(4)
 
+## Generates a definition of a window with a simple window and a single button to close it
 static func create_single_button_close_popup(window_title: StringName, window_message: StringName, button_text: StringName = "OK", window_minumum_size: Vector2i = Vector2i(0,0)) -> ConfigurablePopupDefinition:
 	var button: ConfigurableButtonDefinition = ConfigurableButtonDefinition.create_close_button_definition(button_text)
 	var buttons: Array[ConfigurableButtonDefinition] = []
 	buttons.append(button)
 	return ConfigurablePopupDefinition.new(window_title, window_message, buttons, window_minumum_size)
+
+func _generate_random_letters(num_letters: int) -> StringName:
+	var result: String = ""
+	for i in range(num_letters):
+		# Generate a random integer between 65 (A) and 90 (Z)
+		var random_ascii: int = randi() % (90 - 65 + 1) + 65
+		var random_letter: String = char(random_ascii)
+		result += random_letter
+	return result
