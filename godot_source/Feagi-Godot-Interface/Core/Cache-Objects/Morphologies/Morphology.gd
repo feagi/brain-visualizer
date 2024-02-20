ca6ae2c80c7bf593bc201941c436b795e9b88304
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-02-20 18:20:13 -0500

    Refactor out MorphologyFactory, add in internal_class definitions for morphology

--- godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Morphologies/Morphology.gd
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/Morphologies/Morphology.gd
@@ -1,11 +1,12 @@
 extends Object
 class_name Morphology
 ## Base morpology class, should not be spawned directly, instead spawn one of the types
-const USER_NONEDITABLE_MORPHOLOGIES: PackedStringArray = ["memory"] # Which morphologies can the user not edit the details of?
+const USER_NONEDITABLE_MORPHOLOGIES_AS_PER_FEAGI: Array[MORPHOLOGY_INTERNAL_CLASS] = [MORPHOLOGY_INTERNAL_CLASS.CORE] # Which morphologies can the user not edit the details of?
 
 signal numerical_properties_updated(self_reference: Morphology)
 signal retrieved_usage(usage_mappings: Array[PackedStringArray], is_being_used: bool, self_reference: Morphology)
 signal retrieved_description(description: StringName, self_reference: Morphology)
+signal internal_class_updated(new_internal_class: MORPHOLOGY_INTERNAL_CLASS)
 signal about_to_be_deleted(self_reference: Morphology)
 
 # Probably redudant to have an enum when we have multiple classes, but here somewhat for legacy reasons
@@ -17,11 +18,18 @@ enum MORPHOLOGY_TYPE {
 	NULL,
 }
 
+enum MORPHOLOGY_INTERNAL_CLASS {
+	CUSTOM,
+	CORE,
+	UNKNOWN
+}
+
 var name: StringName
 var description: StringName # TODO retrieve!
 var type: MORPHOLOGY_TYPE
+var internal_class: MORPHOLOGY_INTERNAL_CLASS # Will ALWAYS be CORE if data is placeholder
 var is_placeholder_data: bool
-var is_user_editable: bool = true ## if true, morphology cannot be edited or deleted
+var is_user_editable: bool = true ## if false, morphology cannot be edited or deleted
 var usage_by_cortical_area: Array[PackedStringArray]: ## May be out of date, be sure to poll latest when needed
 	get: 
 		return _usage_by_cortical_area
@@ -34,14 +42,49 @@ var is_being_used: bool:
 
 var _usage_by_cortical_area: Array[PackedStringArray] = []
 
-func _init(morphology_name: StringName, is_using_placeholder_data: bool):
+func _init(morphology_name: StringName, is_using_placeholder_data: bool, feagi_defined_internal_class: MORPHOLOGY_INTERNAL_CLASS):
 	name = morphology_name
 	is_placeholder_data = is_using_placeholder_data
-	is_user_editable = !morphology_name in USER_NONEDITABLE_MORPHOLOGIES
+	internal_class = feagi_defined_internal_class
+	is_user_editable = !internal_class in USER_NONEDITABLE_MORPHOLOGIES_AS_PER_FEAGI
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_PREDELETE:
 		about_to_be_deleted.emit(self) # Notify all others about deletion
+
+## Spawns correct morphology type given dict from FEAGI
+static func create(name: StringName, morphology_type: MORPHOLOGY_TYPE, feagi_defined_internal_class: MORPHOLOGY_INTERNAL_CLASS, morphology_details: Dictionary) -> Morphology:
+	match morphology_type:
+		Morphology.MORPHOLOGY_TYPE.FUNCTIONS:
+			return FunctionMorphology.new(name, false, feagi_defined_internal_class, morphology_details["parameters"])
+		Morphology.MORPHOLOGY_TYPE.VECTORS:
+			return VectorMorphology.new(name, false, feagi_defined_internal_class, FEAGIUtils.array_of_arrays_to_vector3i_array(morphology_details["vectors"]))
+		Morphology.MORPHOLOGY_TYPE.PATTERNS:
+			return PatternMorphology.new(name, false, feagi_defined_internal_class, PatternVector3Pairs.raw_pattern_nested_array_to_array_of_PatternVector3s(morphology_details["patterns"]))
+		Morphology.MORPHOLOGY_TYPE.COMPOSITE:
+			return CompositeMorphology.new(name, false, feagi_defined_internal_class, FEAGIUtils.array_to_vector3i(morphology_details["src_seed"]), FEAGIUtils.array_of_arrays_to_vector2i_array(morphology_details["src_pattern"]), morphology_details["mapper_morphology"])
+		_:
+			# Something else? Error out
+			@warning_ignore("assert_always_false")
+			assert(false, "Invalid Morphology attempted to spawn")
+			return NullMorphology.new()
+
+## creates a morphology object but fills data with placeholder data until FEAGI responds
+static func create_placeholder(name: StringName, type: MORPHOLOGY_TYPE) -> Morphology:
+	match type:
+		Morphology.MORPHOLOGY_TYPE.FUNCTIONS:
+			return FunctionMorphology.new(name, true, MORPHOLOGY_INTERNAL_CLASS.UNKNOWN, {})
+		Morphology.MORPHOLOGY_TYPE.VECTORS:
+			return VectorMorphology.new(name, true, MORPHOLOGY_INTERNAL_CLASS.UNKNOWN, [])
+		Morphology.MORPHOLOGY_TYPE.PATTERNS:
+			return PatternMorphology.new(name, true, MORPHOLOGY_INTERNAL_CLASS.UNKNOWN, [])
+		Morphology.MORPHOLOGY_TYPE.COMPOSITE:
+			return CompositeMorphology.new(name, true, MORPHOLOGY_INTERNAL_CLASS.UNKNOWN, Vector3i(1,1,1), [], "NOT_SET")
+		_:
+			# Something else? Error out
+			@warning_ignore("assert_always_false")
+			assert(false, "Invalid Morphology attempted to spawn")
+			return NullMorphology.new()
 
 static func morphology_array_to_string_array_of_names(morphologies: Array[Morphology]) -> Array[StringName]:
 	var output: Array[StringName] = []
@@ -58,3 +101,14 @@ func feagi_update_usage(feagi_raw_input: Array[Array]) -> void:
 	for mapping: Array in feagi_raw_input:
 		_usage_by_cortical_area.append(PackedStringArray([mapping[0], mapping[1]]))
 	retrieved_usage.emit(_usage_by_cortical_area, is_being_used, self)
+
+## Called by FEAGI when updating a morphology definition (when type is consistent)
+func feagi_update(_parameter_value: Dictionary, retrieved_internal_class: MORPHOLOGY_INTERNAL_CLASS) -> void:
+	# extend in child classes
+	is_placeholder_data = false
+	if retrieved_internal_class != internal_class:
+		internal_class = retrieved_internal_class
+		internal_class_updated.emit(internal_class)
+	numerical_properties_updated.emit(self)
+	
+
--- godot_source/Feagi-Godot-Interface/Core/Cache-Objects/MorphologiesCache.gd
+++ godot_source/Feagi-Godot-Interface/Core/Cache-Objects/MorphologiesCache.gd
@@ -15,43 +15,28 @@ var _available_morphologies: Dictionary = {}
 func add_morphology_by_dict(properties: Dictionary) -> void:
 	var morphology_name: StringName = properties["morphology_name"]
 	var morphology_type: Morphology.MORPHOLOGY_TYPE  = properties["type"]
+	var morphology_internal_class: Morphology.MORPHOLOGY_INTERNAL_CLASS
+	if "internal_class" in properties.keys():
+		morphology_internal_class = properties["internal_class"]
+	else:
+		morphology_internal_class = Morphology.MORPHOLOGY_INTERNAL_CLASS.CUSTOM
 	
 	if morphology_name in available_morphologies.keys():
 		push_error("Attempted to create already cached morphology " + morphology_name + ", Skipping!")
 		return
-	_available_morphologies[morphology_name] = MorphologyFactory.create(morphology_name, morphology_type, properties)
+	_available_morphologies[morphology_name] = Morphology.create(morphology_name, morphology_type, morphology_internal_class, properties)
 	morphology_added.emit(_available_morphologies[morphology_name])
 
-#TODO make this more consistant to how cortical areas are done!
+## Retrieved updated info of morphology from FEAGI
 func update_morphology_by_dict(morphology_properties: Dictionary) -> void:
 	var morphology_name: StringName = morphology_properties["morphology_name"]
 	if morphology_name not in _available_morphologies.keys():
 		push_error("Attemped to update non-cached morphology %s, Skipping..." % [morphology_properties["morphology_name"]])
 		return
-	var morphology_type: Morphology.MORPHOLOGY_TYPE = Morphology.MORPHOLOGY_TYPE[morphology_properties["type"].to_upper()]
-	match morphology_type:
-		Morphology.MORPHOLOGY_TYPE.PATTERNS:
-			var raw_pattern_array: Array[Array] = []
-			raw_pattern_array.assign(morphology_properties["parameters"]["patterns"])  # manual array casting
-			_available_morphologies[morphology_name].patterns = PatternVector3Pairs.raw_pattern_nested_array_to_array_of_PatternVector3s(raw_pattern_array)
-		Morphology.MORPHOLOGY_TYPE.VECTORS:
-			var raw_vector3_array: Array[Array] = []
-			raw_vector3_array.assign(morphology_properties["parameters"]["vectors"]) # manual array casting
-			_available_morphologies[morphology_name].vectors = FEAGIUtils.array_of_arrays_to_vector3i_array(raw_vector3_array)
-		Morphology.MORPHOLOGY_TYPE.FUNCTIONS:
-			_available_morphologies[morphology_name].parameters = morphology_properties["parameters"]
-		Morphology.MORPHOLOGY_TYPE.COMPOSITE:
-			_available_morphologies[morphology_name].source_seed = FEAGIUtils.array_to_vector3i(morphology_properties["parameters"]["src_seed"])
-			var raw_source_pattern_array: Array[Array] = []
-			raw_source_pattern_array.assign(morphology_properties["parameters"]["src_pattern"]) # manual array casting
-			_available_morphologies[morphology_name].source_pattern = FEAGIUtils.array_of_arrays_to_vector2i_array(raw_source_pattern_array)
-			_available_morphologies[morphology_name].mapper_morphology_name = morphology_properties["parameters"]["mapper_morphology"]
-		_:
-			push_error("Unknown Morphology Type! Skipping update!")
-			return
-	_available_morphologies[morphology_name].is_placeholder_data = false
-	_available_morphologies[morphology_name].numerical_properties_updated.emit(_available_morphologies[morphology_name]) #TODO NO
-	morphology_updated.emit(_available_morphologies[morphology_name])
+	var updating_morphology: Morphology = _available_morphologies[morphology_name]
+	var morphology_internal_class: Morphology.MORPHOLOGY_INTERNAL_CLASS = Morphology.MORPHOLOGY_INTERNAL_CLASS[morphology_properties["class"].to_upper()]
+	updating_morphology.feagi_update(morphology_properties["parameters"], morphology_internal_class)
+	morphology_updated.emit(updating_morphology)
 
 ## Should only be called by FEAGI - removes a morphology by name
 func remove_morphology(morphology_Name: StringName) -> void:
@@ -63,7 +48,6 @@ func remove_morphology(morphology_Name: StringName) -> void:
 	_available_morphologies.erase(morphology_Name)
 	deleting.free()
 	
-
 ## Removes all morphologies from cache. Should only be called during a reset
 func hard_wipe_cached_morphologies():
 	print("CACHE: Wiping morphologies...")
@@ -103,7 +87,7 @@ func update_morphology_cache_from_summary(_new_listing_with_types: Dictionary) -
 	for add in added:
 		# since we only have a input dict with the name and type of morphology, we need to generate placeholder objects
 		var adding_type: Morphology.MORPHOLOGY_TYPE = Morphology.MORPHOLOGY_TYPE[(_new_listing_with_types[add].to_upper())]
-		var adding_morphology: Morphology = MorphologyFactory.create_placeholder(add, adding_type)
+		var adding_morphology: Morphology = Morphology.create_placeholder(add, adding_type)
 		_available_morphologies[add] = adding_morphology
 		morphology_added.emit(adding_morphology)
 	
