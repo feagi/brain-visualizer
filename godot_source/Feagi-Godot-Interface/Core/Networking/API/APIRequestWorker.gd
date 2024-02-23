a35e27e595a3896423bf229641679d56819f8753
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-02-22 19:49:15 -0500

    Added type enforcement

--- godot_source/Feagi-Godot-Interface/Core/Networking/API/APIRequestWorker.gd
+++ godot_source/Feagi-Godot-Interface/Core/Networking/API/APIRequestWorker.gd
@@ -192,10 +192,9 @@ func _http_generic_error_response_handling(response_body: PackedByteArray) -> vo
 		## If feagi didnt even send back the dict correctly, something went very wrong
 		#TODO action?
 		return
-	if VisConfig.visualizer_state != VisConfig.STATES.READY:
-		return
+	var temp_test: StringName = feagi_error_response["code"]
 	#VisConfig.UI_manager.make_error_notification(feagi_error_response["code"], _http_error_replacements)
-	VisConfig.UI_manager.make_error_notification(feagi_error_response["code"], {})
+	VisConfig.UI_manager.make_error_notification(temp_test, {})
 
 ## If space is available in the [RequestWorker] pool, add self to the end there
 ## Otherwise, destroy self
