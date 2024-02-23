5f150f50ac3cf1fd033460312f6aece0d6abb823
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-02-22 19:18:15 -0500

    remove dict

--- godot_source/Feagi-Godot-Interface/Core/Networking/API/APIRequestWorker.gd
+++ godot_source/Feagi-Godot-Interface/Core/Networking/API/APIRequestWorker.gd
@@ -192,7 +192,8 @@ func _http_generic_error_response_handling(response_body: PackedByteArray) -> vo
 		## If feagi didnt even send back the dict correctly, something went very wrong
 		#TODO action?
 		return
-	VisConfig.UI_manager.make_error_notification(feagi_error_response["code"], _http_error_replacements)
+	#VisConfig.UI_manager.make_error_notification(feagi_error_response["code"], _http_error_replacements)
+	VisConfig.UI_manager.make_error_notification(feagi_error_response["code"], {})
 
 ## If space is available in the [RequestWorker] pool, add self to the end there
 ## Otherwise, destroy self
