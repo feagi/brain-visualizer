bc974ea8a4007776d8eaeedce13b5f5f35ab50a0
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-02-27 18:15:44 -0500

    Scaling Text Button, add some UI scaling to Top bar

---
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Elements/UIScaling/ScalingTextButton.gd
@@ -0,0 +1,15 @@
+extends Control
+class_name ScalingTextButton
+## Keeps the Button to scale with its initial custom_minimum_size
+
+var _default_font_size: int
+
+func _ready() -> void:
+	_default_font_size = get_theme_font_size(&"font_size")
+	_update_size(VisConfig.UI_manager.UI_scale)
+	VisConfig.UI_manager.UI_scale_changed.connect(_update_size)
+
+func _update_size(multiplier: float) -> void:
+	add_theme_font_size_override(&"font_size", int(float(_default_font_size) * multiplier))
+	size = Vector2(0,0)
+	
