fd8cc3341d23180f54dda4b29488a1ce9795d8fd
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-02-27 14:16:29 -0500

    Have the scaling options actually set scale

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/User_Options/WindowUserOptions.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/User_Options/WindowUserOptions.gd
@@ -3,3 +3,8 @@ class_name WindowUserOptions
 
 func setup() -> void:
 	_setup_base_window("user_options")
+	var scale_multiplier_field: FloatInput = $'BoxContainer/Size Scaling/FloatInput'
+	scale_multiplier_field.current_float = VisConfig.UI_manager.UI_scale
+
+func _user_change_scale(scale_multiplier: float) -> void:
+	VisConfig.UI_manager.UI_scale = scale_multiplier
