2528ac41f8201fa5cbb6f3cbb1acae1bc95cb056
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-02-27 14:17:17 -0500

    Update Container Shrinker to attempt to shrink following all resize events

--- godot_source/Feagi-Godot-Interface/UI/Windows/Elements/UIScaling/ContainerShrinker.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Elements/UIScaling/ContainerShrinker.gd
@@ -2,6 +2,9 @@ extends BoxContainer
 class_name ContainerShrinker
 ## Holds input function that triggers a size recalculation for all contained
 
+func _ready() -> void:
+	VisConfig.UI_manager.UI_scale_changed.connect(recalculate_size.unbind(1))
+
 ## Any events that should cause a size recalculation should be connected via signal here
 func recalculate_size() -> void:
 	size = Vector2(0,0) # This is terrible. Too bad!
