7838c05dbbbb6a1f45a6cb2e5051ee1a8f48fd59
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-03-04 19:44:51 -0500

    port dev options to new window

--- godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Developer_Options/WindowDeveloperOptions.gd
+++ godot_source/Feagi-Godot-Interface/UI/Windows/Draggable_Windows/Developer_Options/WindowDeveloperOptions.gd
@@ -5,6 +5,6 @@ var _camera_animation_section: VerticalCollapsible
 
 func setup() -> void:
 	_setup_base_window("developer_options")
-	_camera_animation_section = $BoxContainer/Camera_Animation
+	_camera_animation_section = _window_internals.get_node("Camera_Animation")
 	
 	_camera_animation_section.setup()
