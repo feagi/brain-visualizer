236f19941f6b83bbee449eba24ede3752bdf5c46
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-04-02 15:59:50 -0400

    made stats warnings to print in console

--- godot_source/addons/FeagiCoreIntegration/FeagiCore/Networking/API/FEAGIHTTPResponses.gd
+++ godot_source/addons/FeagiCoreIntegration/FeagiCore/Networking/API/FEAGIHTTPResponses.gd
@@ -15,13 +15,14 @@ static func GET_healthCheck(response_body: PackedByteArray, _irrelevant_data: Va
 # special version of the above function used for validating if feagi is active and working, used for feagi connection attempt
 static func GET_healthCheck_FEAGI_VALIDATION(response_body: PackedByteArray, _irrelevant_data: Variant):
 	print("response!")
-
+	push_warning("response!")
 
 static func GET_healthCheck_FEAGI_VALIDATION_ERROR(_response_body: PackedByteArray, _request_definition: APIRequestWorkerDefinition):
 	print("Error")
 	
 static func GET_healthCheck_FEAGI_VALIDATION_UNRESPONSIVE(_request_definition: APIRequestWorkerDefinition):
 	print("NO CONNECTION")
+	push_warning("NO CONNECTION")
 	
 #endregion
 
