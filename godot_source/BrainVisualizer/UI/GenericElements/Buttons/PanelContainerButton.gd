cd51e1a473b04eba9d9353bfcda20d2f0d034ddb
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-04-08 16:24:18 -0400

    Completed new themeing system

---
+++ godot_source/BrainVisualizer/UI/GenericElements/Buttons/PanelContainerButton.gd
@@ -0,0 +1,41 @@
+extends PanelContainer
+class_name PanelContainerButton
+
+signal pressed()
+
+var _unpressed: StyleBoxFlat = StyleBoxFlat.new()
+var _hover: StyleBoxFlat = StyleBoxFlat.new()
+var _clicked: StyleBoxFlat = StyleBoxFlat.new()
+
+
+func _ready() -> void:
+	mouse_entered.connect(_mouse_entered)
+	mouse_exited.connect(_mouse_exited)
+
+## Called externally from [ScaleThemeApplier]
+func update_theme(standard, hover, pressed) -> void:
+		_unpressed = standard
+		_hover = hover
+		_clicked = pressed
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		var mouse_event: InputEventMouseButton = event as InputEventMouseButton
+		if mouse_event.button_index != MOUSE_BUTTON_LEFT:
+			return
+		
+		if mouse_event.pressed:
+			add_theme_stylebox_override("panel", _clicked)
+			print("press")
+			pressed.emit()
+		else:
+			add_theme_stylebox_override("panel", _hover)
+			print("hover")
+		
+func _mouse_entered() -> void:
+	add_theme_stylebox_override("panel", _hover)
+	print("hover")
+
+func _mouse_exited() -> void:
+	add_theme_stylebox_override("panel", _unpressed)
+	print("exit")
--- godot_source/BrainVisualizer/UI/GenericElements/ScaleThemeApplier.gd
+++ godot_source/BrainVisualizer/UI/GenericElements/ScaleThemeApplier.gd
@@ -4,6 +4,7 @@ class_name ScaleThemeApplier
 
 var _nodes_to_not_include_or_search: Array[Node] ## when searching and this node is encountered, stop. We may have custom things here instead
 var _texture_buttons: Array[TextureButton] = []
+var _PanelContainerButtons: Array[PanelContainerButton] = []
 
 ## Call this when all children were added to the tree so the reference arrays can be built
 func setup(starting_node: Node, nodes_to_not_include_or_search: Array[Node], current_loaded_theme: Theme) -> void:
@@ -21,25 +22,39 @@ func search_for_matching_children(starting_node: Node) -> void:
 		if child is TextureButton:
 			_texture_buttons.append(child)
 		
+		elif child is PanelContainerButton:
+			_PanelContainerButtons.append(child)
+		
 		search_for_matching_children(child) # Recusrion!
 
 ## Applies custom data changes from new theme to all cached references
 func update_theme_customs(updated_theme: Theme) -> void:
 	var size_x: int = 0
 	var size_y: int = 0
-	
-	
-	
+	var box_normal: StyleBoxFlat
+	var box_pressed: StyleBoxFlat
+	var box_hover: StyleBoxFlat
+
 	# TextureButton
 	if updated_theme.has_constant("size_x", "TextureButton") && updated_theme.has_constant("size_y", "TextureButton"):
 		size_x = updated_theme.get_constant("size_x", "TextureButton")
 		size_y = updated_theme.get_constant("size_y", "TextureButton")
+		for texture_button: TextureButton in _texture_buttons:
+			if texture_button == null:
+				continue
+			texture_button.custom_minimum_size = Vector2i(size_x, size_y)
 	else:
 		push_error("THEME: Loaded theme file is missing size_x and/or size_y for TextureButton. There will be sizing issues!")
 	
-	for texture_button: TextureButton in _texture_buttons:
-		if texture_button == null:
-			continue
-		texture_button.custom_minimum_size = Vector2i(size_x, size_y)
-		
-			
+	# PanelContainerButton
+	if updated_theme.has_stylebox("panel", "PanelContainerButton") and updated_theme.has_stylebox("panel_hover", "PanelContainerButton") and updated_theme.has_stylebox("panel_pressed", "PanelContainerButton"):
+		box_normal = updated_theme.get_stylebox("panel", "PanelContainerButton")
+		box_hover = updated_theme.get_stylebox("panel_hover", "PanelContainerButton")
+		box_pressed = updated_theme.get_stylebox("panel_pressed", "PanelContainerButton")
+		for container_button: PanelContainerButton in _PanelContainerButtons:
+			if container_button == null:
+				continue
+			container_button.update_theme(box_normal, box_hover, box_pressed)
+	else:
+		push_error("THEME: Loaded theme file is missing styleboxes for PanelContainerButton. There will be coloring issues!")
+
