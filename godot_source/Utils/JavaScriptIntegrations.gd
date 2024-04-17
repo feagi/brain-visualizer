f2f187ecc2a32b70a76d787abbb304c9f937f76f
kakcalu13 <kevin.a.araujo@gmail.com>
2024-04-17 17:29:37 -0400

    reverted and updated better name

--- godot_source/Utils/JavaScriptIntegrations.gd
+++ godot_source/Utils/JavaScriptIntegrations.gd
@@ -21,8 +21,20 @@ static func grab_feagi_endpoint_details() -> FeagiEndpointDetails:
 	#TODO: Double check this carefully in NRS
 	var websocket_port: int = 9050
 	var http_port: int =  8000
+	var feagi_web_port: int
+	var feagi_socket_port: int
+	var feagi_TLD: StringName
+	var feagi_SSL: StringName
+	var feagi_socket_SSL: StringName
+	var feagi_root_web_address: StringName
+	var feagi_root_websocket_address: StringName
+	var feagi_socket_address: StringName
+	var DEF_FEAGI_TLD: StringName = "127.0.0.1" # Default localhost
+	var DEF_FEAGI_SSL: StringName = "http://" # Default localhost
 	
-	var tld_result = JavaScriptBridge.eval(""" 
+	# if full_dns_of_websocket is used, websocket will ignore ip_address_to_connect
+	# otherwise it will use this variable. 
+	var ip_address_to_connect = JavaScriptBridge.eval(""" 
 		function getIPAddress() {
 			var url_string = window.location.href;
 			var url = new URL(url_string);
@@ -32,7 +44,7 @@ static func grab_feagi_endpoint_details() -> FeagiEndpointDetails:
 		}
 		getIPAddress();
 		""")
-	var is_using_standard_port = JavaScriptBridge.eval(""" 
+	var without_port = JavaScriptBridge.eval(""" 
 		function get_port() {
 			var url_string = window.location.href;
 			var url = new URL(url_string);
@@ -42,7 +54,7 @@ static func grab_feagi_endpoint_details() -> FeagiEndpointDetails:
 		}
 		get_port();
 		""")
-	var websocket_tld = JavaScriptBridge.eval(""" 
+	var full_dns_of_websocket = JavaScriptBridge.eval(""" 
 		function get_port() {
 			var url_string = window.location.href;
 			var url = new URL(url_string);
@@ -52,7 +64,7 @@ static func grab_feagi_endpoint_details() -> FeagiEndpointDetails:
 		}
 		get_port();
 		""")
-	var http_type_str = JavaScriptBridge.eval(""" 
+	var SSL_type = JavaScriptBridge.eval(""" 
 		function get_port() {
 			var url_string = window.location.href;
 			var url = new URL(url_string);
@@ -62,21 +74,31 @@ static func grab_feagi_endpoint_details() -> FeagiEndpointDetails:
 		}
 		get_port();
 		""")
-	
-	if http_type_str == null or tld_result == null or is_using_standard_port == null or websocket_tld == null:
-		# Something didnt return correctly. Return empty FeagiEndpointDetails
-		return FeagiEndpointDetails.create_from("", 0, "", 0, false)
-	
-	
-	var is_encrypted: bool = str(http_type_str).to_lower() == "https://"
-	if str(is_using_standard_port).to_lower() == "true":
-		# We are using a standard port
-		# In the case of standard ports, NRS sets up routing
-		if is_encrypted:
-			http_port = 443
-			websocket_port = 443
+	feagi_web_port = http_port
+	feagi_socket_port = websocket_port
+	if SSL_type != null:
+		feagi_SSL = SSL_type
+	else:
+		feagi_SSL= DEF_FEAGI_SSL
+	if ip_address_to_connect != null:
+		feagi_TLD = ip_address_to_connect
+	else:
+		feagi_TLD = DEF_FEAGI_TLD
+	if without_port != null:
+		if without_port.to_lower() == "true":
+			feagi_root_web_address = feagi_SSL + feagi_TLD
 		else:
-			http_port = 80
-			websocket_port = 80
+				feagi_root_web_address = feagi_SSL + feagi_TLD + ":" + str(feagi_web_port)
+	else:
+		feagi_root_web_address = feagi_SSL + feagi_TLD + ":" + str(feagi_web_port)
+		# init WebSocket
+	if full_dns_of_websocket != null:
+		feagi_socket_address = full_dns_of_websocket
+	else:
+		feagi_socket_address = feagi_socket_SSL + feagi_TLD + ":" + str(feagi_socket_port)
+		
+	print("websocket: ", feagi_socket_address, " and api: ", feagi_root_web_address)
 	
-	return FeagiEndpointDetails.create_from(str(tld_result), http_port, str(websocket_tld), websocket_port, is_encrypted)
+	# We need to update below. That's it. This is perfect for everything include playground, cloud and all that. dont change. 
+	return FeagiEndpointDetails.create_from(str(ip_address_to_connect), http_port, str(ip_address_to_connect), websocket_port, true)
+	#return FeagiEndpointDetails.create_from(str(tld_result), http_port, str(websocket_tld), websocket_port, is_encrypted)
