c9e78583e8c5080ae20c04ba3b4f34ad58595446
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-05-09 18:58:43 -0400

    brain region cache

---
+++ godot_source/addons/FeagiCoreIntegration/FeagiCore/LocalCache/BrainRegionsCache.gd
@@ -0,0 +1,34 @@
+extends RefCounted
+class_name BrainRegionsCache
+## Holds a local copy of all [BrainRegion]s
+
+signal region_added(region: BrainRegion)
+signal region_about_to_be_removed(region: BrainRegion)
+
+var available_brain_regions: Dictionary:
+	get: return _available_brain_regions
+
+var _available_brain_regions: Dictionary = {}
+
+func FEAGI_add_regions_from_new_genome(data: Dictionary) -> void:
+	pass
+
+func FEAGI_add_region(region_ID: StringName, region_name: StringName, coord_2D: Vector2i, coord_3D: Vector3i, 
+	dim_3D: Vector3i, contained_areas: Array[BaseCorticalArea], region_inputs: Dictionary, 
+	region_outputs: Dictionary, containing_regions: Array[BrainRegion]):
+	
+	var region: BrainRegion = BrainRegion.new(region_ID, region_name, coord_2D, coord_3D, 
+		dim_3D, contained_areas, region_inputs, region_outputs)
+	region.init_contained_regions(containing_regions)
+	_available_brain_regions[region_ID] = region
+	region_added.emit(region)
+
+func FEAGI_remove_region(region_ID: StringName) -> void:
+	if !(region_ID in _available_brain_regions.keys()):
+		push_error("CORE CACHE: Unable to find region %s to delete! Skipping!" % region_ID)
+	var region: BrainRegion = _available_brain_regions[region_ID]
+	region.FEAGI_delete_this_region()
+	region_about_to_be_removed.emit(region)
+	_available_brain_regions.erase(region_ID)
+
+
