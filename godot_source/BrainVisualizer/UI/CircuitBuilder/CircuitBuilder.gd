c37399c87a0c9d008403b29ba11a45c646ff65c5
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-05-20 18:43:31 -0400

    new CB start

---
+++ godot_source/BrainVisualizer/UI/CircuitBuilder/CircuitBuilder.gd
@@ -0,0 +1,44 @@
+extends GraphEdit
+class_name CircuitBuilder
+## A 2D Node based representation of a specific Genome Region
+
+@export var move_time_delay_before_update_FEAGI: float = 5.0
+@export var initial_position: Vector2
+@export var initial_zoom: float
+@export var keyboard_movement_speed: Vector2 = Vector2(1,1)
+@export var keyboard_move_speed: float = 50.0
+
+
+var representing_region: BrainRegion:
+	get: return _representing_region
+var cortical_nodes: Dictionary:## All cortical nodes on CB, key'd by their cortical ID 
+	get: return  _cortical_nodes 
+var subregion_nodes: Dictionary: ## All subregion nodes on CB, key'd by their region ID
+	get: return _subregion_nodes
+
+var _cortical_nodes: Dictionary = {}
+var _subregion_nodes: Dictionary = {}
+var _representing_region: BrainRegion
+
+func setup(region: BrainRegion) -> void:
+	_representing_region = region
+	
+	for area: BaseCorticalArea in _representing_region.contained_cortical_areas:
+		FEAGI_add_cortical_area(area)
+	
+	for subregion: BrainRegion in _representing_region.contained_regions:
+		FEAGI_add_subregion(subregion)
+	
+
+func FEAGI_add_cortical_area(area: BaseCorticalArea) -> void:
+	print("adding " + area.cortical_ID)
+	
+
+func FEAGI_remove_cortical_area(area: BaseCorticalArea) -> void:
+	pass
+
+func FEAGI_add_subregion(subregion: BrainRegion) -> void:
+	print("adding " + subregion.ID)
+
+func FEAGI_remove_subregion(subregion: BrainRegion) -> void:
+	pass
