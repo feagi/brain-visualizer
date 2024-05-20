b1a276e2877bb0367f5f7170420c2ca6884134cf
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-05-20 15:59:41 -0400

    MultiTabView

---
+++ godot_source/BrainVisualizer/UI/TabbedView/MultiTabView.gd
@@ -0,0 +1,30 @@
+extends PanelContainer
+class_name MultiTabView
+## Holds a split container that can show 2 sets of [TabContainer]s. Secondary window starts disabled
+
+var primary_tabs: TabContainer
+var secondary_tabs: TabContainer
+
+var _split_container: SplitContainer
+
+
+func _ready():
+	_split_container = $SplitContainer
+	primary_tabs = $SplitContainer/Primary/TabContainer
+	secondary_tabs = $SplitContainer/Primary/TabContainer
+
+func setup_with_1_control_set(primary_control_set: Array[Control]) -> void:
+	for control in primary_control_set:
+		primary_tabs.add_child(control)
+	
+
+
+func toggle_secondary_visibility(is_second_visible: bool) -> void:
+	_split_container.collapsed = !is_second_visible
+
+func set_split_vertical(should_be_vertical: bool) -> void:
+	_split_container.vertical = should_be_vertical
+
+func is_split_vertical() -> bool:
+	return _split_container.vertical
+
