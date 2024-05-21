d3e080e16ebdc706b4b5088133338abbb4113e18
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-05-21 12:13:55 -0400

    move multitab view

--- godot_source/BrainVisualizer/UI/MultiTabbedView/MultiTabView.gd
+++ godot_source/BrainVisualizer/UI/MultiTabbedView/MultiTabView.gd
@@ -11,12 +11,15 @@ var _split_container: SplitContainer
 func _ready():
 	_split_container = $SplitContainer
 	primary_tabs = $SplitContainer/Primary/TabContainer
-	secondary_tabs = $SplitContainer/Primary/TabContainer
+	secondary_tabs = $SplitContainer/Secondary/TabContainer
 
 func setup_with_1_control_set(primary_control_set: Array[Control]) -> void:
 	for control in primary_control_set:
 		primary_tabs.add_child(control)
+		if control is CircuitBuilder:
+			BV.UI.root_multi_tab_view.CB_register(control as CircuitBuilder)
 	
+	$SplitContainer/Secondary.visible = false
 
 
 func toggle_secondary_visibility(is_second_visible: bool) -> void:
