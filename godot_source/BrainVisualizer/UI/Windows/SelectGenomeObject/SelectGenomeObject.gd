424085ce8a22fbd039bcdd27a97f04920ae3df17
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-05-27 19:52:17 -0400

    Window to select regions

---
+++ godot_source/BrainVisualizer/UI/Windows/SelectGenomeObject/SelectGenomeObject.gd
@@ -0,0 +1,11 @@
+extends BaseDraggableWindow
+class_name SelectGenomeObject
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
