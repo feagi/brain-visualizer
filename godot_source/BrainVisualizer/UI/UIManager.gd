33b38b3761d9fb878b3ba86f17a63ad0d97cfc79
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-07-01 18:33:04 -0400

    fix loading screen crash on reload

--- godot_source/BrainVisualizer/UI/UIManager.gd
+++ godot_source/BrainVisualizer/UI/UIManager.gd
@@ -91,7 +91,8 @@ func FEAGI_confirmed_genome() -> void:
 	initial_tabs = [cb]
 	_root_UI_view.set_this_as_root_view()
 	_root_UI_view.setup_as_single_tab(initial_tabs)
-	$TempLoadingScreen.queue_free()
+	if $TempLoadingScreen != null:
+		$TempLoadingScreen.queue_free()
 	
 
 #endregion
