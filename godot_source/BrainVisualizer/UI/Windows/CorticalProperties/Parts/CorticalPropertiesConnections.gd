f802d25999890461ff1a84e20ac049f47bfccda7
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-07-01 18:08:53 -0400

    cortical properties window can show recursives

--- godot_source/BrainVisualizer/UI/Windows/CorticalProperties/Parts/CorticalPropertiesConnections.gd
+++ godot_source/BrainVisualizer/UI/Windows/CorticalProperties/Parts/CorticalPropertiesConnections.gd
@@ -3,16 +3,22 @@ class_name CorticalPropertiesConnections
 
 var _scroll_afferent: ScrollSectionGeneric
 var _scroll_efferent: ScrollSectionGeneric
+var _recursive: Button
 var _cortical_area_ref: AbstractCorticalArea
 
 func _ready() -> void:
 	_scroll_afferent = $Afferents
 	_scroll_efferent = $Efferents
+	_recursive = $recursive
 
 
 ## Get initial connections when the window is created
 func initial_values_from_FEAGI(cortical_reference: AbstractCorticalArea) -> void:
 	_cortical_area_ref = cortical_reference
+	
+	# Recursive
+	for recursive_area: AbstractCorticalArea in cortical_reference.recursive_mappings.keys():
+		_add_recursive_area(recursive_area)
 	
 	# Inputs
 	for afferent_area: AbstractCorticalArea in cortical_reference.afferent_mappings.keys():
@@ -23,10 +29,17 @@ func initial_values_from_FEAGI(cortical_reference: AbstractCorticalArea) -> void
 		_add_efferent_area(efferent_area)
 		efferent_area.efferent_input_cortical_area_removed.connect(_remove_efferent_area)
 	
+	cortical_reference.recursive_cortical_area_added.connect(_add_recursive_area)
+	cortical_reference.recursive_cortical_area_added.connect(_remove_recursive_area)
 	cortical_reference.afferent_input_cortical_area_added.connect(_add_afferent_area)
 	cortical_reference.efferent_input_cortical_area_added.connect(_add_efferent_area)
 	cortical_reference.afferent_input_cortical_area_removed.connect(_remove_afferent_area)
 	cortical_reference.efferent_input_cortical_area_removed.connect(_remove_efferent_area)
+	_recursive.pressed.connect(_user_pressed_recursive_button)
+	
+
+func _add_recursive_area(area: AbstractCorticalArea, _irrelevant_mapping = null) -> void:
+	_recursive.text = "Recursive Connection"
 
 func _add_afferent_area(area: AbstractCorticalArea, _irrelevant_mapping = null) -> void:
 	var call_mapping_window: Callable = BV.WM.spawn_mapping_editor.bind(area, _cortical_area_ref)
@@ -66,17 +79,18 @@ func _add_efferent_area(area: AbstractCorticalArea, _irrelevant_mapping = null) 
 	var popup_request: Callable = BV.WM.spawn_popup.bind(delete_popup)
 	item.get_delete_button().pressed.connect(popup_request)
 
+
+func _remove_recursive_area(area: AbstractCorticalArea, _irrelevant_mapping = null) -> void:
+	_recursive.text = "None Recursive"
+
 func _remove_afferent_area(area: AbstractCorticalArea, _irrelevant_mapping = null) -> void:
 	_scroll_afferent.attempt_remove_item(area)
 
 func _remove_efferent_area(area: AbstractCorticalArea, _irrelevant_mapping = null) -> void:
 	_scroll_efferent.attempt_remove_item(area)
 
-func _remove_efferent_connection(efferent_area: AbstractCorticalArea):
-	_scroll_efferent.remove_child_by_name(efferent_area.cortical_ID)
-
-func _remove_afferent_connection(afferent_area: AbstractCorticalArea):
-	_scroll_afferent.remove_child_by_name(afferent_area.cortical_ID)
+func _user_pressed_recursive_button() -> void:
+	BV.WM.spawn_mapping_editor(_cortical_area_ref, _cortical_area_ref)
 
 func _user_pressed_add_afferent_button() -> void:
 	BV.WM.spawn_mapping_editor(null, _cortical_area_ref)
