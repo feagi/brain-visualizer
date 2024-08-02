ce7336a6062072269662b380278b468d835f9e22
Amir-Rasteg <amir.rasteg@protonmail.com>
2024-08-01 23:58:49 -0400

    update websocket API

--- godot_source/addons/FeagiCoreIntegration/FeagiCore/Networking/WebSocket/FEAGIWebSocketAPI.gd
+++ godot_source/addons/FeagiCoreIntegration/FeagiCore/Networking/WebSocket/FEAGIWebSocketAPI.gd
@@ -1,70 +1,91 @@
 extends Node
 class_name FEAGIWebSocketAPI
 
+enum WEBSOCKET_HEALTH {
+	NO_CONNECTION,
+	CONNECTED,
+	RETRYING
+}
+
 const DEF_SOCKET_INBOUND_BUFFER_SIZE: int = 10000000
 const DEF_SOCKET_BUFFER_SIZE: int = 10000000
 const DEF_PING_INTERVAL_SECONDS: float = 2.0
 const SOCKET_GENOME_UPDATE_FLAG: String = "updated" # FEAGI sends this string via websocket if genome is reloaded / changed
-const SOCKET_GENEOME_UPDATE_LATENCY: String = "ping"
+const SOCKET_GENEOME_UPDATE_LATENCY: String = "ping" # TODO DELETE
 
-signal socket_state_changed(state: WebSocketPeer.State)
-signal feagi_return_ping()
+signal FEAGI_socket_health_changed(previous_health: WEBSOCKET_HEALTH, current_health: WEBSOCKET_HEALTH)
+signal FEAGI_socket_retrying_connection(retry_count: int, max_retry_count: int)
 signal feagi_requesting_reset()
 signal feagi_return_other(data: Variant)
 
-var _cache_websocket_data: PackedByteArray # outside to void reallocation penalties
+var _cache_websocket_data: PackedByteArray # outside to try to avoid reallocation penalties # NOTE: Godot doesnt seem to care and reallocates anyways lol
+var _socket_web_address: StringName = ""
 var _socket: WebSocketPeer
-var _socket_prev_state: WebSocketPeer.State = WebSocketPeer.State.STATE_CLOSED
+var _socket_health: WEBSOCKET_HEALTH = WEBSOCKET_HEALTH.NO_CONNECTION
+var _retry_count: int = 0
+var _is_purposfully_disconnecting: bool = false
 
-func _process(delta: float):
+func _process(_delta: float):
 	_socket.poll()
-	_refresh_socket_state()
-	match(_socket_prev_state):
+	match(_socket.get_ready_state()):
 		WebSocketPeer.State.STATE_CONNECTING:
 			# Currently connecting to feagi, waiting for FEAGI to confirm
 			pass
 		WebSocketPeer.State.STATE_OPEN:
 			# Connection active with FEAGI
+			if _socket_health != WEBSOCKET_HEALTH.CONNECTED:
+				_set_socket_health(WEBSOCKET_HEALTH.CONNECTED)
+				_retry_count = 0
+			
 			while _socket.get_available_packet_count():
 				_cache_websocket_data = _socket.get_packet().decompress(DEF_SOCKET_BUFFER_SIZE, 1) # for some reason, using the enum instead of the number causes this break
 				if _cache_websocket_data.get_string_from_utf8() == SOCKET_GENOME_UPDATE_FLAG: # This isn't particuarly efficient. Too bad!
 					print("FEAGI Websocket: Recieved Request from FEAGI to reset genome!")
 					feagi_requesting_reset.emit()
-				elif _cache_websocket_data.get_string_from_utf8() == SOCKET_GENEOME_UPDATE_LATENCY:
-					feagi_return_ping.emit()
+				elif _cache_websocket_data.get_string_from_utf8() == SOCKET_GENEOME_UPDATE_LATENCY: # TODO DELETE
+					return
 				else:
 					# assume its visualization data
 					var temp = str_to_var(_cache_websocket_data.get_string_from_ascii())
-					#if temp ==  null:
-					#	return
 					feagi_return_other.emit(temp)
-					
 
 		WebSocketPeer.State.STATE_CLOSING:
 			# Closing connection to FEAGI, waiting for FEAGI to respond to close request
 			pass
 		WebSocketPeer.State.STATE_CLOSED:
 			# Closed Connection to FEAGI
-			var close_code: int = _socket.get_close_code()
-			var close_reason: String = _socket.get_close_reason()
 			if  _socket.get_available_packet_count() > 0:
 				# There was some remenant data
 				_cache_websocket_data = _socket.get_packet().decompress(DEF_SOCKET_BUFFER_SIZE, 1)
 			#TODO FeagiEvents.retrieved_visualization_data.emit(str_to_var(_cache_websocket_data.get_string_from_ascii())) # Add to erase neurons
-			push_warning("FEAGI Websocket: Closed with code: %d, reason %s. Clean: %s" % [close_code, close_reason, close_code != -1])
-			set_process(false)
+			if _is_purposfully_disconnecting:
+				_is_purposfully_disconnecting = false
+				_set_socket_health(WEBSOCKET_HEALTH.NO_CONNECTION)
+				set_process(false)
+				return
+			if _retry_count < FeagiCore.feagi_settings.number_of_times_to_retry_WS_connections:
+				if _socket_health != WEBSOCKET_HEALTH.RETRYING:
+					_set_socket_health(WEBSOCKET_HEALTH.RETRYING)
+				FEAGI_socket_retrying_connection.emit(_retry_count, FeagiCore.feagi_settings.number_of_times_to_retry_WS_connections)
+				_reconnect_websocket()
+				_retry_count += 1
+				return
+			else:
+				_set_socket_health(WEBSOCKET_HEALTH.NO_CONNECTION)
+				set_process(false)
 
-## Initializes and attempts to concect the websocket
-func connect_websocket(feagi_socket_address: StringName) -> void:
-	print("attempt connect")
-	if _socket == null:
-		_socket =  WebSocketPeer.new()
-		_socket.inbound_buffer_size = DEF_SOCKET_INBOUND_BUFFER_SIZE
-	_refresh_socket_state()
-	if _socket_prev_state != WebSocketPeer.STATE_CLOSED:
-		push_warning("FEAGI Websocket: Cannot initate a new connection when websocket is not closed!")
-		return
-	_socket.connect_to_url(feagi_socket_address)
+## Inits address needed to connect
+func setup(feagi_socket_address: StringName) -> void:
+	_socket_web_address = feagi_socket_address
+
+## Starts a connection
+func connect_WS() -> void:
+	if _socket_web_address == "":
+		push_error("FEAGI WS: No address specified!")
+	_is_purposfully_disconnecting = false
+	_retry_count = 0
+	set_process(true)
+	_reconnect_websocket()
 
 ## Force closes the socket. This does cause 'socket_state_changed' to fire
 func disconnect_websocket() -> void:
@@ -74,24 +95,17 @@ func disconnect_websocket() -> void:
 
 ## attempts to send data over websocket
 func websocket_send(data: Variant) -> void:
-	if _socket_prev_state != WebSocketPeer.STATE_OPEN:
+	if _socket_health != WEBSOCKET_HEALTH.CONNECTED:
 		push_warning("FEAGI Websocket: Unable to send data to closed socket!")
 		return
 	_socket.send((data.to_ascii_buffer()).compress(1)) # for some reason, using the enum instead of the number causes this break
 
-# attempts to send a ping over websocket
-func send_websocket_ping() -> void:
-	websocket_send(SOCKET_GENEOME_UPDATE_LATENCY)
+func _reconnect_websocket() -> void:
+	_socket = null # enforce dereference
+	_socket =  WebSocketPeer.new()
+	_socket.inbound_buffer_size = DEF_SOCKET_INBOUND_BUFFER_SIZE
+	_socket.connect_to_url(_socket_web_address)
 
-func get_websocket_state() -> WebSocketPeer.State:
-	if _socket != null:
-		return _socket.get_ready_state()
-	return WebSocketPeer.State.STATE_CLOSED # If socket isnt existing, then it must be closed
-
-func _refresh_socket_state() -> void:
-	if _socket_prev_state == _socket.get_ready_state():
-		## no change, doesn't matter which we return
-		return
-	_socket_prev_state = _socket.get_ready_state()
-	print(_socket_prev_state)
-	socket_state_changed.emit(_socket_prev_state)
+func _set_socket_health(new_health: WEBSOCKET_HEALTH) -> void:
+	var prev_health: WEBSOCKET_HEALTH = _socket_health
+	FEAGI_socket_health_changed.emit(prev_health, new_health)
