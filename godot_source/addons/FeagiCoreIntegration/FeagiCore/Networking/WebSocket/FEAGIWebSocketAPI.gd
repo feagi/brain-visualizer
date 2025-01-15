5990d359ef41c0696c1a516899683849e5b33ac4
Amir-Rasteg <Amir.Rasteg@protonmail.com>
2025-01-15 18:27:10 -0500

    start using binary neuron activation location data

--- godot_source/addons/FeagiCoreIntegration/FeagiCore/Networking/WebSocket/FEAGIWebSocketAPI.gd
+++ godot_source/addons/FeagiCoreIntegration/FeagiCore/Networking/WebSocket/FEAGIWebSocketAPI.gd
@@ -45,20 +45,32 @@ func _process(_delta: float):
 			while _socket.get_available_packet_count():
 				_cache_websocket_data = _socket.get_packet().decompress(DEF_SOCKET_BUFFER_SIZE, 1) # for some reason, using the enum instead of the number causes this break
 				
-				# dumb support for wrapped jsons right now
-				if _cache_websocket_data[0] == 1: # json wrapped! Ignore version as I doubt that will ever change
-					_cache_websocket_data = _cache_websocket_data.slice(2)
-					var dict: Dictionary = str_to_var(_cache_websocket_data.get_string_from_ascii()) 
-					if !dict:
-						push_error("FEAGI: Unable to parse WS Data!")
-						return
-					if dict.has("status"):
-						var dict_status = dict["status"]
-						FeagiCore.feagi_local_cache.update_health_from_FEAGI_dict(dict_status)
-						if dict_status.has("genome_changed"):
-							feagi_requesting_reset.emit()
-					if dict.has("activations"):
-						feagi_return_other.emit(dict["activations"])
+				## respond as per type
+				match(_cache_websocket_data[0]):
+					1: # JSON wrapper
+						_cache_websocket_data = _cache_websocket_data.slice(2)
+						var dict: Dictionary = str_to_var(_cache_websocket_data.get_string_from_ascii()) 
+						if !dict:
+							push_error("FEAGI: Unable to parse WS Data!")
+							return
+						if dict.has("status"):
+							var dict_status = dict["status"]
+							FeagiCore.feagi_local_cache.update_health_from_FEAGI_dict(dict_status)
+							if dict_status.has("genome_changed"):
+								feagi_requesting_reset.emit()
+					7: # ActivatedNeuronLocation
+						# TODO we really shouldnt be doing processing in here
+						# This array processing is slow. However to resolve this, we need to PR godot to create a proper method to handle this
+						var activations: Array[Array] = []
+						var offset: int = 2 # skip header
+						var max_offset: int = len(_cache_websocket_data)
+						while offset < max_offset:
+							activations.append([_cache_websocket_data.decode_s16(offset), _cache_websocket_data.decode_s16(offset + 2), _cache_websocket_data.decode_s16(offset + 4)])
+							offset += 6
+						feagi_return_other.emit(activations)
+				
+				
+
 				
 		WebSocketPeer.State.STATE_CLOSING:
 			# Closing connection to FEAGI, waiting for FEAGI to respond to close request
