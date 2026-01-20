f9d55494470b8b7b37cf4af0b54bee6c63fa94ce
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2026-01-19 23:24:12 -0500

    bv package handling

---
+++ feagi-bv-platform/feagi-bv-linux/feagi_bv_linux/__init__.py
@@ -0,0 +1,8 @@
+"""
+FEAGI Brain Visualizer binaries for Linux.
+
+This package contains Linux x86_64 binaries for Brain Visualizer.
+Install via: pip install feagi-bv (auto-installs this package on Linux)
+"""
+
+__version__ = "2.0.0"
