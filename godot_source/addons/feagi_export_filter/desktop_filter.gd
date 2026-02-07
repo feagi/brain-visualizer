fc69885cd7ffa0d877d0472ea20f270a4e10b606
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2026-02-07 15:02:29 -0500

    exclusion of feagi embedded on remote builds

--- godot_source/addons/feagi_export_filter/desktop_filter.gd
+++ godot_source/addons/feagi_export_filter/desktop_filter.gd
@@ -7,6 +7,7 @@ func _get_name() -> String:
 func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> void:
 	print("[FEAGI Export Filter] Desktop export started")
 	print("[FEAGI Export Filter] Filtering out data deserializer extension")
+	print("[FEAGI Export Filter] Filtering out feagi_embedded (Remote mode - embedded not needed)")
 
 func _export_file(path: String, type: String, features: PackedStringArray) -> void:
 	# Skip the data deserializer extension file - it's only needed for web
@@ -14,7 +15,13 @@ func _export_file(path: String, type: String, features: PackedStringArray) -> vo
 	if path == "res://addons/feagi_rust_deserializer/feagi_data_deserializer.gdextension":
 		skip()
 		print("[FEAGI Export Filter] Skipped: ", path)
-	
+
+	# Skip feagi_embedded addon - Remote mode connects to external FEAGI, does not need in-process embedded
+	# Saves ~6MB on PyPI package size
+	if path.begins_with("res://addons/feagi_embedded/"):
+		skip()
+		return
+
 	# Skip legacy Feagi-Godot-Interface BVVersion to avoid class name conflict
 	# The correct BVVersion is in BrainVisualizer/BVVersion.gd
 	if path == "res://Feagi-Godot-Interface/Core/NonCache-Objects/BVVersion.gd":
