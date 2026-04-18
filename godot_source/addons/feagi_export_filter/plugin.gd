1da6f2834942b33dd8fbed3571dd5060cb8a2f32
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2026-04-18 10:25:13 -0400

    fixed timing issue on deploy

--- godot_source/addons/feagi_export_filter/plugin.gd
+++ godot_source/addons/feagi_export_filter/plugin.gd
@@ -1,15 +1,28 @@
 @tool
 extends EditorPlugin
 
-var _web_filter := preload("res://addons/feagi_export_filter/web_filter.gd").new()
-var _desktop_filter := preload("res://addons/feagi_export_filter/desktop_filter.gd").new()
+var _web_filter: EditorExportPlugin = null
+var _desktop_filter: EditorExportPlugin = null
 
 func _enter_tree() -> void:
+	if _web_filter == null:
+		_web_filter = preload("res://addons/feagi_export_filter/web_filter.gd").new()
+	if _desktop_filter == null:
+		_desktop_filter = preload("res://addons/feagi_export_filter/desktop_filter.gd").new()
 	add_export_plugin(_web_filter)
 	add_export_plugin(_desktop_filter)
 	print("[FEAGI Export Filter] Plugin loaded")
 
 func _exit_tree() -> void:
-	remove_export_plugin(_web_filter)
-	remove_export_plugin(_desktop_filter)
+	# In headless CI export mode, shutdown order can invalidate export plugin internals
+	# during teardown, causing native aborts after a successful export. We skip explicit
+	# remove in that environment and let process teardown release instances.
+	if DisplayServer.get_name() == "headless":
+		return
+	if _web_filter != null:
+		remove_export_plugin(_web_filter)
+	_web_filter = null
+	if _desktop_filter != null:
+		remove_export_plugin(_desktop_filter)
+	_desktop_filter = null
 
