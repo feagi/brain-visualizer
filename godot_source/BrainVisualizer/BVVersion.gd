3fda905a330303e9e8125fd59b56d8287925447c
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2026-09-02 00:40:05 -0400

    version bump

--- godot_source/BrainVisualizer/BVVersion.gd
+++ godot_source/BrainVisualizer/BVVersion.gd
@@ -3,6 +3,6 @@ class_name BVVersion
 ## NOTE: This class is updated by github actions, what you see here isnt what you get!
 
 static var manual_version: String:
-	get: return "2.3.26"
+	get: return "2.3.34"
 static var brain_visualizer_timestamp: int: # set by github actions
-	get: return 1788323778
+	get: return 1788323991
